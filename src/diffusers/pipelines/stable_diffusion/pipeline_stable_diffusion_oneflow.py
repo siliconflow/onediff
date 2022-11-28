3fb6cfec96f7b2cd472ddeacf078aaa0bee7407a
liujuncheng <liujuncheng1022@gmail.com>
2022-11-28 21:13:00 +0800

    refine

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_oneflow.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_oneflow.py
@@ -28,6 +28,8 @@ os.environ["ONEFLOW_KERNEL_ENABLE_FUSED_LINEAR"] = "1"
 os.environ["ONEFLOW_MLIR_GROUP_MATMUL"] = "1"
 os.environ["ONEFLOW_MLIR_CSE"] = "1"
 os.environ["ONEFLOW_MLIR_FUSE_FORWARD_OPS"] = "1"
+os.environ["ONEFLOW_MATMUL_ALLOW_HALF_PRECISION_ACCUMULATION"] = "1"
+os.environ["ONEFLOW_KERNEL_EANBLE_DUAL_GEMM_GLU"] = "1"
 
 import oneflow as flow
 class UNetGraph(flow.nn.Graph):
