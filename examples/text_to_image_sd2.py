847c07bd2bdc9086befb83dd0f9e95f8c3a2f6f6
jackalcooper <jackalcooper@gmail.com>
2023-02-17 16:10:43 +0800

    add sd2

---
+++ examples/text_to_image_sd2.py
@@ -0,0 +1,15 @@
+import oneflow as flow
+flow.mock_torch.enable()
+from diffusers import EulerDiscreteScheduler
+from onediff import OneFlowStableDiffusionPipeline
+model_id = "stabilityai/stable-diffusion-2"
+
+# Use the Euler scheduler here instead
+scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = OneFlowStableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, revision="fp16", torch_dtype=flow.float16)
+pipe = pipe.to("cuda")
+
+prompt = "a photo of an astronaut riding a horse on mars"
+image = pipe(prompt, height=768, width=768).images[0]
+
+image.save("astronaut_rides_horse.png")
