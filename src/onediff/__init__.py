f536dde192be9c534e599a565af86ae720343d2e
luyang <flowingsun007@163.com>
2023-02-21 09:05:51 +0000

    mock_torch_support_stable_diffusion_inpaint_pipeline

--- src/onediff/__init__.py
+++ src/onediff/__init__.py
@@ -5,3 +5,4 @@ import oneflow as flow
 flow.mock_torch.enable()
 from .pipeline_stable_diffusion_oneflow import OneFlowStableDiffusionPipeline
 from .pipeline_alt_diffusion_oneflow import OneFlowAltDiffusionPipeline
+from .pipeline_stable_diffusion_inpaint_oneflow import OneFlowStableDiffusionInpaintPipeline
