d544353e2a9b9537a4a2c0f78bd7c0d9f32a783b
Luyang <flowingsun007@163.com>
2023-02-21 18:40:02 +0800

    Mock torch support stable diffusion inpaint pipeline (#119)
    - [x] mock_torch 支持stable diffusion inpaint pipeline
    - [x] test example：`python3 examples/text_to_image_inpaint.py`
    - [x]
    对torch.strided、torch.randn的hack迁移至src/onediff/__init__.py，迁移后OneFlowStableDiffusionPipeline、OneFlowStableDiffusionInpaintPipeline均可以正常跑通
    
    ![Face of a yellow cat, high resolution, sitting on a park
    bench-of-0](https://user-images.githubusercontent.com/28823622/220320117-258a9e36-1167-435c-87e0-0f6e3be8cc03.png)

---
+++ examples/text_to_image_inpaint.py
@@ -0,0 +1,30 @@
+import oneflow as flow
+flow.mock_torch.enable()
+
+import PIL
+import requests
+from io import BytesIO
+from onediff import OneFlowStableDiffusionInpaintPipeline
+
+
+def download_image(url):
+    response = requests.get(url)
+    return PIL.Image.open(BytesIO(response.content)).convert("RGB")
+
+
+img_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo.png"
+mask_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo_mask.png"
+init_image = download_image(img_url).resize((512, 512))
+mask_image = download_image(mask_url).resize((512, 512))
+
+pipe = OneFlowStableDiffusionInpaintPipeline.from_pretrained(
+    "runwayml/stable-diffusion-inpainting",
+    torch_dtype=flow.float16,
+)
+pipe = pipe.to("cuda")
+prompt = "Face of a yellow cat, high resolution, sitting on a park bench"
+
+with flow.autocast("cuda"):
+    images = pipe(prompt=prompt, image=init_image, mask_image=mask_image).images
+    for i, image in enumerate(images):
+        image.save(f"{prompt}-of-{i}.png")
--- src/onediff/__init__.py
+++ src/onediff/__init__.py
@@ -2,6 +2,20 @@ __version__ = "0.2.0"
 __author__ = "OneFlow"
 __credits__ = "OneFlow contributors"
 import oneflow as flow
+
+
+# monkey patch hacks
+flow.strided = None
+flow_randn = flow.randn
+
+def dummy_randn(*args, **kwargs):
+    kwargs = {k: v for k, v in kwargs.items() if not k == "layout"}
+    return flow_randn(*args, **kwargs)
+
+flow.randn = dummy_randn
+
+
 flow.mock_torch.enable()
 from .pipeline_stable_diffusion_oneflow import OneFlowStableDiffusionPipeline
 from .pipeline_alt_diffusion_oneflow import OneFlowAltDiffusionPipeline
+from .pipeline_stable_diffusion_inpaint_oneflow import OneFlowStableDiffusionInpaintPipeline
