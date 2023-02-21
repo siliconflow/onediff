1e78ccd60de70cfbfb5a123e65ec815b1b8d756b
luyang <flowingsun007@163.com>
2023-02-21 06:04:27 +0000

    mock_torch_support_alt_diffusion_pipiline

--- src/onediff/__init__.py
+++ src/onediff/__init__.py
@@ -4,3 +4,4 @@ __credits__ = "OneFlow contributors"
 import oneflow as flow
 flow.mock_torch.enable()
 from .pipeline_stable_diffusion_oneflow import OneFlowStableDiffusionPipeline
+from .pipeline_alt_diffusion_oneflow import OneFlowAltDiffusionPipeline
