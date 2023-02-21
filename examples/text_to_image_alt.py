ee3c862792e9b87ca34b56c4f368f06735addc6f
Luyang <flowingsun007@163.com>
2023-02-21 14:14:45 +0800

    Mock torch support alt diffusion pipiline (#117)
    - [x] 以mock torch方式支持AltDiffusion
    - [x] 提供example，执行：`python3 examples/text_to_image_alt.py`
    
    ---------
    
    Co-authored-by: Shenghang Tsai <jackalcooper@gmail.com>

---
+++ examples/text_to_image_alt.py
@@ -0,0 +1,13 @@
+import oneflow as flow
+
+flow.mock_torch.enable()
+from onediff import OneFlowAltDiffusionPipeline
+
+
+pipe = OneFlowAltDiffusionPipeline.from_pretrained("BAAI/AltDiffusion-m9", torch_dtype=flow.float16)
+pipe = pipe.to("cuda")
+prompt = "黑暗精灵公主，非常详细，幻想，非常详细，数字绘画，概念艺术，敏锐的焦点，插图"
+with flow.autocast("cuda"):
+    images = pipe(prompt).images
+    for i, image in enumerate(images):
+        image.save(f"{prompt}-of-{i}.png")
