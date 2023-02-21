358089ac6553e7146d9d72ca5befdd0e6f732138
Luyang <flowingsun007@163.com>
2023-02-21 14:13:12 +0800

    Update examples/text_to_image_alt.py
    Co-authored-by: Shenghang Tsai <jackalcooper@gmail.com>

--- examples/text_to_image_alt.py
+++ examples/text_to_image_alt.py
@@ -7,7 +7,6 @@ from onediff import OneFlowAltDiffusionPipeline
 pipe = OneFlowAltDiffusionPipeline.from_pretrained("BAAI/AltDiffusion-m9", torch_dtype=flow.float16)
 pipe = pipe.to("cuda")
 prompt = "黑暗精灵公主，非常详细，幻想，非常详细，数字绘画，概念艺术，敏锐的焦点，插图"
-# prompt = "Astronaut riding a horse on Mars."
 with flow.autocast("cuda"):
     images = pipe(prompt).images
     for i, image in enumerate(images):
