0460f3e355236d594225415a16a8c3cfa1049d52
daquexian <daquexian566@gmail.com>
2023-02-27 20:23:25 +0800

    overwrite _accelerate_available to False
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- src/onediff/__init__.py
+++ src/onediff/__init__.py
@@ -18,6 +18,8 @@ flow.randn = dummy_randn
 
 
 flow.mock_torch.enable()
+import diffusers
+diffusers.utils.import_utils._accelerate_available = False
 from .pipeline_stable_diffusion_oneflow import OneFlowStableDiffusionPipeline
 from .pipeline_alt_diffusion_oneflow import OneFlowAltDiffusionPipeline
 from .pipeline_stable_diffusion_inpaint_oneflow import (
