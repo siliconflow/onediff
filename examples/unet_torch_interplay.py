b3696775aac2c68a71bd0cd3dcc3e6c2d6062191
strint <xiaoyulink@gmail.com>
2023-03-15 05:13:01 +0000

    add save and load and refine

--- examples/unet_torch_interplay.py
+++ examples/unet_torch_interplay.py
@@ -37,77 +37,83 @@ def mock_wrapper(f):
             del sys.modules[x]
     return ret
 
-
-class UNetGraph(flow.nn.Graph):
-    @flow.nn.Graph.with_dynamic_input_shape(size=9)
-    def __init__(self, unet):
-        super().__init__()
-        self.unet = unet
-        self.config.enable_cudnn_conv_heuristic_search_algo(False)
-        self.config.allow_fuse_add_to_output(True)
-
-    def build(self, latent_model_input, t, text_embeddings):
-        text_embeddings = flow._C.amp_white_identity(text_embeddings)
-        return self.unet(
-            latent_model_input, t, encoder_hidden_states=text_embeddings
-        ).sample
+class MockCtx(object):
+    def __enter__(self):
+        flow.mock_torch.enable(lazy=True)
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        flow.mock_torch.disable()
+        # TODO: this trick of py mod purging will be removed
+        import sys
+        tmp = sys.modules.copy()
+        for x in tmp:
+            if x.startswith("diffusers"):
+                del sys.modules[x]
 
 
-def get_graph(unet):
+def get_unet(token):
+    from diffusers import UNet2DConditionModel
+    unet = UNet2DConditionModel.from_pretrained(
+        "runwayml/stable-diffusion-v1-5",
+        use_auth_token=token,
+        revision="fp16",
+        torch_dtype=flow.float16,
+        subfolder="unet",
+    )
     with flow.no_grad():
         unet = unet.to("cuda")
-        return UNetGraph(unet)
+    return unet
+
+def get_graph(unet):
+    class UNetGraph(flow.nn.Graph):
+        @flow.nn.Graph.with_dynamic_input_shape(size=9)
+        def __init__(self, unet):
+            super().__init__(enable_get_runtime_state_dict=True)
+            self.unet = unet
+            self.config.enable_cudnn_conv_heuristic_search_algo(False)
+            self.config.allow_fuse_add_to_output(True)
+    
+        def build(self, latent_model_input, t, text_embeddings):
+            text_embeddings = flow._C.amp_white_identity(text_embeddings)
+            return self.unet(
+                latent_model_input, t, encoder_hidden_states=text_embeddings
+            ).sample
+
+    return UNetGraph(unet)
 
 
-class UnetCache:
-    def __init__(self, token, graph_getter, arg_meta_of_sizes):
-        from diffusers import UNet2DConditionModel
-
-        self.unet = UNet2DConditionModel.from_pretrained(
-            "runwayml/stable-diffusion-v1-5",
-            use_auth_token=token,
-            revision="fp16",
-            torch_dtype=flow.float16,
-            subfolder="unet",
-        )
-        self.g = graph_getter(self.unet)
-        self.g.debug(0)
+class GraphUtil(object):
+    def __init__(self, graph):
+        self._g = graph
+        self._g.debug(0)
+    
+    def warmup_with_arg(self, arg_meta_of_sizes):
         for arg_metas in arg_meta_of_sizes:
-            print(f"{arg_metas=}")
+            print(f"warmup {arg_metas=}")
             arg_tensors = [flow.empty(a[0], dtype=a[1]).to("cuda") for a in arg_metas]
-            self.g(*arg_tensors)  # build and warmup
+            self._g(*arg_tensors)  # build and warmup
+    
+    def warmup_with_load(self, file_path):
+        state_dict = flow.load(file_path)
+        self._g.load_runtime_state_dict(state_dict)
 
-    def __call__(self, *arg_tensors):
-        return self.g(*arg_tensors)
+    def save_graph(self, file_path):
+        state_dict = self._g.runtime_state_dict()
+        flow.save(state_dict, file_path)
 
+
+test_seq = [2, 1, 0]
+
+def image_dim(i):
+    return 768 + 128 * i
 
 # TODO: noise shape might change by batch
 def noise_shape(batch_size, num_channels, image_w, image_h):
     sizes = (image_w // 8, image_h // 8)
     return (batch_size, num_channels) + sizes
 
-
-test_seq = [2, 1, 0]
-
-
-def image_dim(i):
-    return 768 + 128 * i
-
-
-@click.command()
-@click.option("--token")
-@click.option("--repeat", default=1000)
-@click.option("--sync_interval", default=50)
-def benchmark(token, repeat, sync_interval):
-    # create a mocked unet graph
-    batch_size = 2
-    num_channels = 4
-
-    graph_cache = mock_wrapper(
-        lambda: UnetCache(
-            token,
-            get_graph,
-            [
+def get_arg_meta_of_sizes(batch_size, num_channels):
+    ret =  [
                 [
                     (
                         noise_shape(
@@ -120,10 +126,33 @@ def benchmark(token, repeat, sync_interval):
                 ]
                 for i in test_seq
                 for j in test_seq
-            ],
-        )
-    )
+            ]
+    return ret
 
+
+
+@click.command()
+@click.option("--token")
+@click.option("--repeat", default=10)
+@click.option("--sync_interval", default=50)
+@click.option("--save", type=bool, default=False)
+@click.option("--load", type=bool, default=False)
+@click.option("--file", type=str, default="./unet_graphs")
+def benchmark(token, repeat, sync_interval, save, load, file):
+    # create a mocked unet graph
+    batch_size = 2
+    num_channels = 4
+    with MockCtx():
+        unet = get_unet(token)
+        unet_graph = get_graph(unet)
+        unet_graph_util = GraphUtil(unet_graph)
+        if load == True :
+            print("warmup_with_load")
+            unet_graph_util.warmup_with_load(file)
+        else:
+            print("warmup_with_arg")
+            unet_graph_util.warmup_with_arg(get_arg_meta_of_sizes(batch_size, num_channels))
+        
     # generate inputs with torch
     from diffusers.utils import floats_tensor
     import torch
@@ -159,7 +188,7 @@ def benchmark(token, repeat, sync_interval):
         import random
 
         noise = random.choice(noise_of_sizes)
-        out = graph_cache(noise, time_step, encoder_hidden_states)
+        out = unet_graph(noise, time_step, encoder_hidden_states)
         # convert to torch tensors
         out = flow.utils.tensor.to_torch(out)
         if r == repeat - 1 or r % sync_interval == 0:
@@ -171,6 +200,10 @@ def benchmark(token, repeat, sync_interval):
     print(
         f"Finish {repeat} steps in {duration:.3f} seconds, average {throughput:.2f}it/s"
     )
+
+    if save:
+        print("save_graph")
+        unet_graph_util.save_graph(file)
 
 
 if __name__ == "__main__":
