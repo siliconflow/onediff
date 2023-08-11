974befe97bfc0d7d0491dc9d713079f0acdd9021
jackalcooper <jackalcooper@gmail.com>
2023-08-12 04:10:55 +0800

    add

--- examples/torch_interpretor.py
+++ examples/torch_interpretor.py
@@ -94,6 +94,7 @@ class ProxySubmodule:
     def __call__(self, *args: Any, **kwargs: Any) -> Any:
         replacement = replace_class(type(self._1f_proxy_submod))
         print_types(args, kwargs)
+        # TODO: why can't we use __call__?
         return replacement.forward(self, *args, **kwargs)
 
 class OneFlowInterpreter(torch.fx.Interpreter):
