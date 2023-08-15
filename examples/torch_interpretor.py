76adfc046415c173f762f5fa7cb6764e8b952f24
jackalcooper <jackalcooper@gmail.com>
2023-08-15 11:02:50 +0800

    rm unused imports

--- examples/torch_interpretor.py
+++ examples/torch_interpretor.py
@@ -20,8 +20,6 @@ from typing import Any, Dict, Iterator, List, Optional, Tuple, Union
 import diffusers.utils.torch_utils
 from onediff.infer_compiler import torchbackend
 
-diffusers.utils.torch_utils.maybe_allow_in_graph = lambda x : x
-
 pipe = StableDiffusionPipeline.from_pretrained(
     "CompVis/stable-diffusion-v1-4",
     use_auth_token=True,
@@ -29,18 +27,7 @@ pipe = StableDiffusionPipeline.from_pretrained(
     torch_dtype=torch.float16,
 )
 
-
-def add_op_from_torch(node):
-    getattr(oneflow, node["functioname"])(node.args)
-
-
-torch._dynamo.config.verbose=True
-
-
-# print(pipe.unet.state_dict().keys())
 pipe.unet = torch.compile(pipe.unet, fullgraph=True, mode="reduce-overhead", backend=torchbackend)
-# pipe.unet = torch.compile(pipe.unet, mode="reduce-overhead", fullgraph=True)
-# pipe.vae = torch.compile(pipe.vae, mode="reduce-overhead", fullgraph=True, backend=my_custom_backend)
 pipe = pipe.to("cuda")
 
 prompt = "a photo of an astronaut riding a horse on mars"
