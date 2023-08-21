2909c924129735d98581241e3fba15aed1868a6f
strint <xiaoyulink@gmail.com>
2023-08-21 08:51:05 +0000

    draft graph run

--- examples/text_to_image_sdxl_fp16.py
+++ examples/text_to_image_sdxl_fp16.py
@@ -18,35 +18,40 @@ parser.add_argument(
 )
 parser.add_argument("--saved_image", type=str, required=False, default="xl-base-out.png")
 parser.add_argument("--seed", type=int, default=1)
+parser.add_argument("--compile", action=argparse.BooleanOptionalAction)
 parser.add_argument("--graph", action=argparse.BooleanOptionalAction)
 args = parser.parse_args()
 
+if args.compile:
+    print("unet is compiled to oneflow.")
+    if args.graph:
+        print("unet is compiled to oneflow graph.")
+
 torch.manual_seed(args.seed)
-print(args.graph)
 
 pipe = StableDiffusionXLPipeline.from_pretrained(
     args.model, torch_dtype=torch.float16, variant=args.variant, use_safetensors=True
 )
 
-if args.graph:
-    os.environ["with_graph"] = "1"
-    os.environ["ONEFLOW_MLIR_CSE"] = "1"
-    os.environ["ONEFLOW_MLIR_ENABLE_INFERENCE_OPTIMIZATION"] = "1"
-    os.environ["ONEFLOW_MLIR_ENABLE_ROUND_TRIP"] = "1"
-    os.environ["ONEFLOW_MLIR_FUSE_FORWARD_OPS"] = "1"
-    os.environ["ONEFLOW_MLIR_FUSE_OPS_WITH_BACKWARD_IMPL"] = "1"
-    # Open this will raise error
-    # os.environ["ONEFLOW_MLIR_GROUP_MATMUL"] = "1"
-    os.environ["ONEFLOW_MLIR_PREFER_NHWC"] = "1"
-    os.environ["ONEFLOW_KERNEL_ENABLE_FUSED_CONV_BIAS"] = "1"
-    os.environ["ONEFLOW_KERNEL_ENABLE_FUSED_LINEAR"] = "1"
-    os.environ["ONEFLOW_KERNEL_CONV_CUTLASS_IMPL_ENABLE_TUNING_WARMUP"] = "1"
-    os.environ["ONEFLOW_KERNEL_CONV_ENABLE_CUTLASS_IMPL"] = "1"
-    os.environ["ONEFLOW_CONV_ALLOW_HALF_PRECISION_ACCUMULATION"] = "1"
-    os.environ["ONEFLOW_MATMUL_ALLOW_HALF_PRECISION_ACCUMULATION"] = "1"
-    os.environ["ONEFLOW_LINEAR_EMBEDDING_SKIP_INIT"] = "1"
-
-pipe.unet = torch.compile(pipe.unet, fullgraph=True, mode="reduce-overhead", backend=torchbackend)
+if args.compile:
+    if args.graph:
+        os.environ["with_graph"] = "1"
+        os.environ["ONEFLOW_MLIR_CSE"] = "1"
+        os.environ["ONEFLOW_MLIR_ENABLE_INFERENCE_OPTIMIZATION"] = "1"
+        os.environ["ONEFLOW_MLIR_ENABLE_ROUND_TRIP"] = "1"
+        os.environ["ONEFLOW_MLIR_FUSE_FORWARD_OPS"] = "1"
+        os.environ["ONEFLOW_MLIR_FUSE_OPS_WITH_BACKWARD_IMPL"] = "1"
+        # Open this will raise error
+        # os.environ["ONEFLOW_MLIR_GROUP_MATMUL"] = "1"
+        os.environ["ONEFLOW_MLIR_PREFER_NHWC"] = "1"
+        os.environ["ONEFLOW_KERNEL_ENABLE_FUSED_CONV_BIAS"] = "1"
+        os.environ["ONEFLOW_KERNEL_ENABLE_FUSED_LINEAR"] = "1"
+        os.environ["ONEFLOW_KERNEL_CONV_CUTLASS_IMPL_ENABLE_TUNING_WARMUP"] = "1"
+        os.environ["ONEFLOW_KERNEL_CONV_ENABLE_CUTLASS_IMPL"] = "1"
+        os.environ["ONEFLOW_CONV_ALLOW_HALF_PRECISION_ACCUMULATION"] = "1"
+        os.environ["ONEFLOW_MATMUL_ALLOW_HALF_PRECISION_ACCUMULATION"] = "1"
+        os.environ["ONEFLOW_LINEAR_EMBEDDING_SKIP_INIT"] = "1"
+    pipe.unet = torch.compile(pipe.unet, fullgraph=True, mode="reduce-overhead", backend=torchbackend)
 
 pipe.to("cuda")
 
