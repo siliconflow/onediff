1371164996c78258f384a085bf783975101a73db
strint <xiaoyulink@gmail.com>
2023-08-31 10:01:31 +0000

    fix warning ONEFLOW_MLIR_ENABLE_INFERENCE_OPTIMIZATION

--- src/onediff/infer_compiler/with_fx_graph.py
+++ src/onediff/infer_compiler/with_fx_graph.py
@@ -4,7 +4,7 @@ import torch.fx as fx
 import oneflow as flow
 from torch.fx.node import map_aggregate
 from typing import Any, Dict, Iterator, List, Optional, Tuple, Union
-from .obj_1f_from_torch import replace_obj, replace_func, replace_class, ProxySubmodule
+from .obj_1f_from_torch import replace_obj, replace_func, _get_module, _get_attr
 
 def fx_node_tranform(gm):
     of_gm = to_of_transform(gm)
@@ -69,6 +69,7 @@ def to_of_transform(
             raise ValueError(f"not valid node type{node.foramt_node()}")
 
     of_gm = flow.fx.GraphModule(name2obj, of_g)
+    of_gm.training = False
     of_gm.graph.lint()
     of_gm.recompile()
     return of_gm
