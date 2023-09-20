7502c13e630f15ba994ed2870e20570a260760cf
tsai <jackalcooper@gmail.com>
2023-09-20 16:03:22 +0800

    fix deps

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     install_requires=[
         "transformers>=4.27.1",
         "diffusers>=0.19.3",
+        "torch>=2",
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
