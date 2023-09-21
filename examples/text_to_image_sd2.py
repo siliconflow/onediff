bf1958235ea872ab84b2c44cb3525af53a45b52d
jackalcooper <jackalcooper@gmail.com>
2023-09-21 16:22:34 +0800

    add

--- examples/text_to_image_sd2.py
+++ examples/text_to_image_sd2.py
@@ -2,9 +2,9 @@ from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 import torch
 from onediff.infer_compiler import oneflow_compile
 
-# you can also use a local file directory like this here
-model_id = "/share_nfs/hf_models/stable-diffusion-2-1"
 model_id = "stabilityai/stable-diffusion-2"
+# you can also use a local file directory like this here
+# model_id = "/share_nfs/hf_models/stable-diffusion-2-1"
 # Use the Euler scheduler here instead
 scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
 pipe = StableDiffusionPipeline.from_pretrained(
