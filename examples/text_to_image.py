226788e4ae941a5aa6ece5bd2894ff7902f42213
jackalcooper <jackalcooper@gmail.com>
2023-09-25 16:19:00 +0800

    add model_id

--- examples/text_to_image.py
+++ examples/text_to_image.py
@@ -1,20 +1,38 @@
+import argparse
 from onediff.infer_compiler import oneflow_compile
 from diffusers import StableDiffusionPipeline
 import oneflow as flow
+import torch
 
-model_id = "runwayml/stable-diffusion-v1-5"
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Simple demo of image generation.")
+    parser.add_argument(
+        "--prompt", type=str, default="a photo of an astronaut riding a horse on mars"
+    )
+    parser.add_argument(
+        "--model_id",
+        type=str,
+        default="runwayml/stable-diffusion-v1-5",
+    )
+    args = parser.parse_args()
+    return args
+
+
+args = parse_args()
+
 
 pipe = StableDiffusionPipeline.from_pretrained(
-    model_id,
+    args.model_id,
     use_auth_token=True,
     revision="fp16",
-    torch_dtype=flow.float16,
+    torch_dtype=torch.float16,
 )
 
 pipe = pipe.to("cuda")
 pipe.unet = oneflow_compile(pipe.unet)
 
-prompt = "a photo of an astronaut riding a horse on mars"
+prompt = args.prompt
 with flow.autocast("cuda"):
     images = pipe(prompt).images
     for i, image in enumerate(images):
