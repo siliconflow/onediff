da95be73946809d8865bf310fae4a5b2d8d8f4cf
Yao Chi <later@usopp.net>
2023-09-28 20:44:53 +0800

    lora refine

--- src/onediff/infer_compiler/temp_fix_compile_impl.py
+++ src/onediff/infer_compiler/temp_fix_compile_impl.py
@@ -11,34 +11,24 @@ class FakeCuda:
     @staticmethod
     def mem_get_info(dev):
         return 1024*1024*1024 , 1024*1024*1024
-    
 
-    # torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=0.0, is_causal=False)
     @staticmethod
-    def scaled_dot_product_attention(query, key, value, attn_mask=None, dropout_p=None, is_causal=False)-> flow.Tensor:
-        "Compute 'Scaled Dot Product Attention'"
-        d_k = query.size(-1)
-            
-        if attn_mask is not None:
-            if attn_mask.dtype == flow.bool:
-                # attn_mask = flow.cast(attn_mask, flow.float32)
-                # new_attn_mask = flow.zeros_like(attn_mask)
-                new_attn_mask =flow.empty(attn_mask.shape, dtype=query.dtype, device=query.device)
-                mask = flow.logical_not(attn_mask)
-                # -std::numeric_limits<double>::infinity()
-                new_attn_mask.masked_fill_(mask, float('-inf'))   
-                attn_mask = new_attn_mask 
-        
-        scores = flow.matmul(query, key.transpose(-2, -1)) \
-                / math.sqrt(d_k)
-        
-        if attn_mask is not None:
-            scores.add_(attn_mask)
-        
-        p_attn = flow.nn.functional.softmax(scores, dim = -1)
+    def scaled_dot_product_attention(query, key, value, attn_mask=None, dropout_p=None, is_causal=False):
+        batch_size, num_heads, hidden_size, head_dim = query.shape
 
-        return flow.matmul(p_attn, value)
-        
+        result = flow._C.fused_multi_head_attention_inference_v2(
+            query=query,
+            query_layout="BHMK",
+            query_head_size=head_dim,
+            key=key,
+            key_layout="BHMK",
+            value=value,
+            value_layout="BHMK",
+            output_layout="BM(HK)",
+            causal=False
+        )
+
+        return result
 
 flow.cuda.current_device = FakeCuda.current_device
 flow.cuda.mem_get_info = FakeCuda.mem_get_info
