6c2bc02c259e6b7c9b6c06513fbe43d124c4caaf
clackhan <han_binbin@163.com>
2023-10-30 15:36:45 +0800

    pkgutil_walk_packages_error

--- src/onediff/infer_compiler/import_tools/finder.py
+++ src/onediff/infer_compiler/import_tools/finder.py
@@ -15,9 +15,7 @@ def import_submodules(package, recursive=True):
     """Import all submodules of a module, recursively, including subpackages"""
     if isinstance(package, str):
         package = importlib.import_module(package)
-
-    for _, name, is_pkg in pkgutil.walk_packages(package.__path__):
-        full_name = package.__name__ + "." + name
+    for _, full_name, is_pkg in pkgutil.walk_packages(package.__path__, package.__name__ + "."):
         try:
             # TODO(): Avoid copy, rename comfy.x.x.x with mocked_comfy.x.x.x
             good_import = importlib.import_module(full_name)
