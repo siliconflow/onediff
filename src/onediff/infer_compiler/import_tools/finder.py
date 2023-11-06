1b94e5fa4c6f604500f0f726c760fecd30d41c73
strint <xiaoyulink@gmail.com>
2023-11-06 11:03:25 +0000

    fix copy same folder in mock

--- src/onediff/infer_compiler/import_tools/finder.py
+++ src/onediff/infer_compiler/import_tools/finder.py
@@ -1,15 +1,23 @@
 import inspect
 import pkgutil
 import importlib
+import time
+import os
 from typing import Dict
 from .copier import PackageCopier
 from .printer import print_red
 
 
+def gen_unique_id():
+    timestamp = int(time.time() * 1000)
+    process_id = os.getpid()
+    # TODO(): refine the unique id
+    # sequence = str(uuid.uuid4())
+    unique_id = f"{timestamp}{process_id}"
+    return unique_id
+
 PREFIX = "mock_"
-SUFFIX = ""
-
-
+SUFFIX = gen_unique_id()
 
 def import_submodules(package, recursive=True):
     """Import all submodules of a module, recursively, including subpackages"""
@@ -66,11 +74,18 @@ def get_classes_in_package(package, base_class=None) -> Dict[str, type]:
     return class_dict
 
 
-def get_mock_cls_name(cls)->str:
+def _format_package_name(package_name):
+    return PREFIX + package_name + SUFFIX
+
+
+def get_mock_cls_name(cls) -> str:
     if isinstance(cls, type):
         cls = f"{cls.__module__}.{cls.__name__}"
-        
-    return PREFIX + cls + SUFFIX
+
+    pkg_name, cls_ = cls.split(".", 1)
+
+    pkg_name = _format_package_name(pkg_name)
+    return f"{pkg_name}.{cls_}"
 
 
 
