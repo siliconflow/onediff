2cfcf255341a63bca10de4ed0078e63a1e99ae37
Houjiang Chen <chenhoujiangcug@gmail.com>
2023-11-10 11:53:02 +0800

    Add sdxl lora example (#327)
    Co-authored-by: FengWen <109639975+ccssu@users.noreply.github.com>
    Co-authored-by: strint <xiaoyulink@gmail.com>

---
+++ examples/text_to_image_sdxl_lora.py
@@ -0,0 +1,23 @@
+from onediff.infer_compiler import oneflow_compile
+from diffusers import DiffusionPipeline
+import torch 
+
+model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+pipe = DiffusionPipeline.from_pretrained(model_id, variant="fp16", torch_dtype=torch.float16).to("cuda")
+lora_model_id = "hf-internal-testing/sdxl-1.0-lora"
+lora_filename = "sd_xl_offset_example-lora_1.0.safetensors"
+pipe.load_lora_weights(lora_model_id, weight_name=lora_filename)
+
+# TODO(): Not fused lora will affect the performance.
+pipe.unet = oneflow_compile(pipe.unet)
+
+generator = torch.manual_seed(0)
+images_fusion = pipe(
+    "masterpiece, best quality, mountain",
+    generator=generator,
+    height=1024,
+    width=1024,
+    num_inference_steps=30,
+).images[0]
+
+images_fusion.save("test_sdxl_lora.png")
