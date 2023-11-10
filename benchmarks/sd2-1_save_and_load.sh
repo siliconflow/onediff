017b95ba91fc29a13073c6a7c68274c125f8c465
“ky” <kouyan080@163.com>
2023-11-10 16:11:21 +0800

    add script to test 2.1 speed

--- benchmarks/sd2-1_save_and_load.sh
+++
@@ -1,4 +0,0 @@
-set -eux
-rm -f unet_graphs
-python3 ../examples/unet_torch_interplay.py --save --model_id /share_nfs/hf_models/stable-diffusion-2-1/
-python3 ../examples/unet_torch_interplay.py --load --model_id /share_nfs/hf_models/stable-diffusion-2-1/
