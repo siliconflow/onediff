6d4aa17d5897b27024dac6ab6772ae7c1f65a45e
strint <xiaoyulink@gmail.com>
2023-11-10 03:49:41 +0000

    add todo fuse

--- examples/text_to_image_sdxl_lora.py
+++ examples/text_to_image_sdxl_lora.py
@@ -8,6 +8,7 @@ lora_model_id = "hf-internal-testing/sdxl-1.0-lora"
 lora_filename = "sd_xl_offset_example-lora_1.0.safetensors"
 pipe.load_lora_weights(lora_model_id, weight_name=lora_filename)
 
+# TODO(): Not fused lora will affect the performance.
 pipe.unet = oneflow_compile(pipe.unet)
 
 generator = torch.manual_seed(0)
