533a69fbc6795e8cb11d57cd6fd87142c325fcc9
kouyan <121762045+onAugust1@users.noreply.github.com>
2023-11-11 14:10:42 +0800

    Update sd2_1_test_load.sh

--- benchmarks/sd2_1_test_load.sh
+++ benchmarks/sd2_1_test_load.sh
@@ -1,4 +1,4 @@
 set -eux
-rm -f unet_graphs
-python3 ../examples/save_and_load_graph.py --model_id /share_nfs/hf_models/stable-diffusion-2-1/ --save
-python3 ../examples/save_and_load_graph.py --model_id /share_nfs/hf_models/stable-diffusion-2-1/ --load
+rm -f unet_compiled
+python3 test_load_speed.py  --save $@
+python3 test_load_speed.py  --load $@
