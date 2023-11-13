f0fa97d3f71f08e5b8d406de2d2915aa63c2e80c
jackalcooper <jackalcooper@gmail.com>
2023-11-13 13:20:53 +0800

    rm demo

--- examples/run_all.sh
+++ examples/run_all.sh
@@ -3,5 +3,4 @@ python3 examples/text_to_image_sdxl.py --base /share_nfs/hf_models/stable-diffus
 python3 benchmarks/stable_diffusion_2_unet.py --model_id=/share_nfs/hf_models/stable-diffusion-2-1
 bash examples/unet_save_and_load.sh --model_id=/share_nfs/hf_models/stable-diffusion-2-1
 python3 examples/text_to_image.py --model_id=/share_nfs/hf_models/models--runwayml--stable-diffusion-v1-5
-python3 -m onediff.demo
 python3 examples/image_to_image.py --model_id=/share_nfs/hf_models/stable-diffusion-2-1
