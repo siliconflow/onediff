12cffd6680d37bece6ea4eeca7d7c807fb84a45e
jackalcooper <jackalcooper@gmail.com>
2023-11-13 16:04:18 +0800

    rm

--- ci/run_all_examples.sh
+++
@@ -1,6 +0,0 @@
-set -ex
-python3 examples/text_to_image.py --model_id=/share_nfs/hf_models/stable-diffusion-v1-5
-python3 examples/image_to_image.py --model_id=/share_nfs/hf_models/stable-diffusion-2-1
-python3 examples/text_to_image_sdxl.py --base /share_nfs/hf_models/stable-diffusion-xl-base-1.0
-python3 benchmarks/stable_diffusion_2_unet.py --model_id=/share_nfs/hf_models/stable-diffusion-2-1
-bash examples/unet_save_and_load.sh --model_id=/share_nfs/hf_models/stable-diffusion-2-1
