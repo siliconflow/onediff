d34f9b40ecc61b105e8358155945d0d7a788d574
jackalcooper <jackalcooper@gmail.com>
2023-11-13 13:40:53 +0800

    refine

--- ci/run_all_examples.sh
+++ ci/run_all_examples.sh
@@ -1,7 +1,7 @@
 set -ex
+python3 -m onediff.demo --model_id=/share_nfs/hf_models/stable-diffusion-v1-5
+python3 examples/image_to_image.py --model_id=/share_nfs/hf_models/stable-diffusion-2-1
 python3 examples/text_to_image_sdxl.py --base /share_nfs/hf_models/stable-diffusion-xl-base-1.0 --compile
 python3 benchmarks/stable_diffusion_2_unet.py --model_id=/share_nfs/hf_models/stable-diffusion-2-1
 bash examples/unet_save_and_load.sh --model_id=/share_nfs/hf_models/stable-diffusion-2-1
 python3 examples/text_to_image.py --model_id=/share_nfs/hf_models/stable-diffusion-v1-5
-python3 -m onediff.demo --model_id=/share_nfs/hf_models/stable-diffusion-v1-5
-python3 examples/image_to_image.py --model_id=/share_nfs/hf_models/stable-diffusion-2-1
