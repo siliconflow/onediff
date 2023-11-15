57a4e37a1e8884be044a158cfeeb98f2a165f144
FengWen <ccsuwen@gmail.com>
2023-11-15 19:14:40 +0800

    refine

--- src/onediff/infer_compiler/transform/manager.py
+++ src/onediff/infer_compiler/transform/manager.py
@@ -16,8 +16,8 @@ def onediff_mock_torch():
     attr_name = "__version__"
     restore_funcs = []  # Backup
     if hasattr(flow, attr_name) and hasattr(torch, attr_name):
-        org_flow_attr = getattr(flow, attr_name)
-        restore_funcs.append(lambda: setattr(flow, attr_name, org_flow_attr))
+        orig_flow_attr = getattr(flow, attr_name)
+        restore_funcs.append(lambda: setattr(flow, attr_name, orig_flow_attr))
         setattr(flow, attr_name, getattr(torch, attr_name))
 
     # https://docs.oneflow.org/master/cookies/oneflow_torch.html
