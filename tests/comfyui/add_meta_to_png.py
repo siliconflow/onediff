ddfae4c90092862bae3aeea215d679034111e227
Yao Chi <later@usopp.net>
2023-11-21 10:50:53 +0000

    refine add_meta_to_png

--- tests/comfyui/add_meta_to_png.py
+++ tests/comfyui/add_meta_to_png.py
@@ -4,8 +4,11 @@ import os
 from PIL import Image
 from PIL.PngImagePlugin import PngInfo
 
+
 def parse_args():
-    parser = argparse.ArgumentParser(description="Add ComfyUI API Workflow to a png file.")
+    parser = argparse.ArgumentParser(
+        description="Add ComfyUI API Workflow to a png file."
+    )
     parser.add_argument(
         "--api",
         type=str,
@@ -24,9 +27,16 @@ def parse_args():
         default="",
         help="The output png filename.",
     )
+    parser.add_argument(
+        "--key",
+        type=str,
+        default="api",
+        help="The key name of the metadata.",
+    )
 
     args = parser.parse_args()
     return args
+
 
 if __name__ == "__main__":
     args = parse_args()
@@ -39,16 +49,17 @@ if __name__ == "__main__":
 
     with open(args.api, "r") as f:
         c = f.read()
-        metadata.add_text("api", c)
-    
+        metadata.add_text(args.key, c)
+
     origin_img.close()
 
     api_img = Image.open(args.png)
 
     if args.output == "":
-        pngname, _ = os.path.splitext(args.png)
-        apiname, _ = os.path.splitext(args.api)
+        pngname, _ = os.path.splitext(os.path.basename(args.png))
+        apiname, _ = os.path.splitext(os.path.basename(args.api))
         output = f"processed-{apiname}-{pngname}.png"
-
+    else:
+        output = args.output
     api_img.save(output, pnginfo=metadata)
     api_img.close()
