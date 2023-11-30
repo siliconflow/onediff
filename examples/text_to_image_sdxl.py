c9ec2e3a141ae37a172e63cd84fecfdbb87965d0
Li Xiang <54010254+lixiang007666@users.noreply.github.com>
2023-11-30 22:22:16 +0800

    Update text_to_image_sdxl.py

--- examples/text_to_image_sdxl.py
+++ examples/text_to_image_sdxl.py
@@ -73,7 +73,7 @@ if args.compile_vae:
 
 # Define multiple resolutions for warmup
 resolutions = (
-    [(512, 512), (256, 256),]
+    [(512, 512), (256, 256), (512, 256)]
     if args.use_multiple_resolutions
     else [(args.height, args.width)]
 )
