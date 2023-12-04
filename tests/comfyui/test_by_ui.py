d452927f29cc8c8bd6de8b19575e305aaed0d23f
Yao Chi <later@usopp.net>
2023-12-04 14:24:33 +0000

    remove COMFYUI_SPEEDUP_DIR env var

--- tests/comfyui/test_by_ui.py
+++ tests/comfyui/test_by_ui.py
@@ -7,7 +7,6 @@ Please set the following environment variables (whose values are for reference o
 export ACR_ORG=registry.cn-beijing.aliyuncs.com/oneflow
 export MATRIX_IMAGE=onediff-pro:cu121
 export COMFYUI_SRC_DIR=ComfyUI
-export COMFYUI_SPEEDUP_DIR=comfyui-speedup
 export SELENIUM_IMAGE=standalone-chrome:119.0-chromedriver-119.0-grid-4.15.0-20231129
 export SELENIUM_CONTAINER_NAME=selenium-test
 export COMFYUI_PORT=8855
