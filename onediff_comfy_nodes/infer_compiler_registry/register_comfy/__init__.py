c7a461f576c1e13421ea87612401b0d1e9d21f2e
Yao Chi <later@usopp.net>
2023-12-12 14:26:38 +0800

    init dev

--- onediff_comfy_nodes/infer_compiler_registry/register_comfy/__init__.py
+++ onediff_comfy_nodes/infer_compiler_registry/register_comfy/__init__.py
@@ -8,11 +8,15 @@ import comfy
 from .attention import CrossAttention as CrossAttention1f
 from .attention import SpatialTransformer as SpatialTransformer1f
 from .linear import Linear as Linear1f
+from .openaimodel import Upsample as Upsample1f
+from .openaimodel import UNetModel as UNetModel1f
 
 torch2of_class_map = {
     comfy.ldm.modules.attention.CrossAttention: CrossAttention1f,
     comfy.ldm.modules.attention.SpatialTransformer: SpatialTransformer1f,
     comfy.ops.Linear: Linear1f,
+    comfy.ldm.modules.diffusionmodules.openaimodel.Upsample: Upsample1f,
+    comfy.ldm.modules.diffusionmodules.openaimodel.UNetModel: UNetModel1f,
 }
 
 
