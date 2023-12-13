4123a7af948115fc45ff85e1817199550e6c457c
Wang Yi <53533850+marigoold@users.noreply.github.com>
2023-12-13 15:09:37 +0800

    add context manager `TensorInplaceAssign` (#396)
    在 diffusers 中，load LoRA 的实现是直接把 fuse 之后的 weight 赋值给
    self.weight（比如[这里](https://github.com/huggingface/diffusers/blob/b21729225a9fc39d793d072dc125cb16fb71c87b/src/diffusers/models/lora.py#L138
    )），这会使赋值后 tensor 的内存地址改变，而 graph 依然使用原来的内存地址，导致 LoRA 无法正确加载。
    这里实现了一个 Tensor 和 Parameter 的子类，重写了 property data 的 getter 和 setter，使其
    set 的时候原地拷贝，不改变内存地址。
    
    正确性验证：
    
    1. 在每个赋值语句后面都加了内存地址不变的断言，没有挂掉
    ```python
            dptr = self.regular_linear_layer.weight.data.data_ptr()
            self.regular_linear_layer.weight.data = fused_weight.to(device=device, dtype=dtype)
            if "AutoInplaceCopy" in str(type(self.regular_linear_layer.weight.data)):
                print(f"got type [{type(self.regular_linear_layer.weight.data)}], shape is {self.regular_linear_layer.weight.data.shape}")
                assert self.regular_linear_layer.weight.data.data_ptr() == dptr
    ```
    2. examples/text_to_image_sdxl_lora.py 脚本中，先用 oneflow graph forward
    一次，再加载 LoRA，结果与 torch 相同；在 oneflow_compile 中删掉对应转换语句，结果与 torch 不同。

--- examples/text_to_image_sdxl_lora.py
+++ examples/text_to_image_sdxl_lora.py
@@ -3,6 +3,7 @@ import torch
 import diffusers
 from diffusers import DiffusionPipeline
 from onediff.infer_compiler import oneflow_compile
+from onediff.infer_compiler.utils import TensorInplaceAssign
 
 
 MODEL_ID = "stabilityai/stable-diffusion-xl-base-1.0"
@@ -11,15 +12,26 @@ pipe = DiffusionPipeline.from_pretrained(
 ).to("cuda")
 LORA_MODEL_ID = "hf-internal-testing/sdxl-1.0-lora"
 LORA_FILENAME = "sd_xl_offset_example-lora_1.0.safetensors"
+
+pipe.unet = oneflow_compile(pipe.unet)
 pipe.load_lora_weights(LORA_MODEL_ID, weight_name=LORA_FILENAME)
+generator = torch.manual_seed(0)
 
 # The 'fuse_lora' API is not available in diffuser versions prior to 0.21.0.
 if hasattr(pipe, "fuse_lora"):
-    pipe.fuse_lora(lora_scale=1.0)
+    with TensorInplaceAssign(pipe.unet):
+        pipe.fuse_lora(lora_scale=1.0)
 
-pipe.unet = oneflow_compile(pipe.unet)
+if hasattr(pipe, "unfuse_lora"):
+    with TensorInplaceAssign(pipe.unet):
+        pipe.unfuse_lora()
 
-generator = torch.manual_seed(0)
+# load LoRA twice to for checking result consistency
+pipe.load_lora_weights(LORA_MODEL_ID, weight_name=LORA_FILENAME)
+if hasattr(pipe, "fuse_lora"):
+    with TensorInplaceAssign(pipe.unet):
+        pipe.fuse_lora(lora_scale=1.0)
+
 images_fusion = pipe(
     "masterpiece, best quality, mountain",
     generator=generator,
