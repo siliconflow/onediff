06ee56ea83ea96331f7b9f1b9be8212c1c350620
Yao Chi <later@usopp.net>
2023-12-13 21:55:05 +0800

    refine, detect the community version

--- onediff_comfy_nodes/infer_compiler_registry/register_comfy/__init__.py
+++ onediff_comfy_nodes/infer_compiler_registry/register_comfy/__init__.py
@@ -1,4 +1,5 @@
 from onediff.infer_compiler import register
+from onediff.infer_compiler.utils import is_community_version
 from nodes import *  # must imported before import comfy
 from pathlib import Path
 
@@ -8,8 +9,6 @@ import comfy
 from .attention import CrossAttention as CrossAttention1f
 from .attention import SpatialTransformer as SpatialTransformer1f
 from .linear import Linear as Linear1f
-from .openaimodel import Upsample as Upsample1f
-from .openaimodel import UNetModel as UNetModel1f
 
 if hasattr(comfy.ops, "disable_weight_init"):
     comfy_ops_Linear = comfy.ops.disable_weight_init.Linear
@@ -23,9 +22,20 @@ torch2of_class_map = {
     comfy.ldm.modules.attention.CrossAttention: CrossAttention1f,
     comfy.ldm.modules.attention.SpatialTransformer: SpatialTransformer1f,
     comfy_ops_Linear: Linear1f,
-    comfy.ldm.modules.diffusionmodules.openaimodel.Upsample: Upsample1f,
-    comfy.ldm.modules.diffusionmodules.openaimodel.UNetModel: UNetModel1f,
 }
+
+if not is_community_version():
+    from .openaimodel import Upsample as Upsample1f
+    from .openaimodel import UNetModel as UNetModel1f
+
+    torch2of_class_map.update(
+        {
+            comfy.ldm.modules.diffusionmodules.openaimodel.Upsample: Upsample1f,
+            comfy.ldm.modules.diffusionmodules.openaimodel.UNetModel: UNetModel1f,
+        }
+    )
+else:
+    print("Dynamic batchsize is not supported in community version.")
 
 
 register(torch2oflow_class_map=torch2of_class_map)
