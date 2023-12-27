9f4ad5f77d6f7cc2179fc1dc801ea08e7869cae2
Jianhua Zheng <zhengjianhua@oneflow.org>
2023-12-27 15:14:12 +0800

    support v1-5-pruned model in sd-webui (#463)
    主要改动
    * 修复 [issue: webui
    切换模型会报错](https://github.com/siliconflow/onediff/issues/435)。v1-5-pruned
    模型对应的代码是 lgm 下的 UNetModel，原先并不支持其编译。
    * 重构代码，将编译相关的代码从 Script 中拆出来。
    * sd-webui 在加载插件时，将对应 extension（而不是 scripts） 的目录放到 sys.path。现在可以将代码拆开了。
      * 因为 ldm、sgm、script 都需要访问 model，而 model 只能作为全局变量，所以只能放到一个单独的文件中。
      * 将 ldm、sgm 的 unet 编译分别放到单独的文件。

---
+++ onediff_sd_webui_extensions/compile_sgm.py
@@ -0,0 +1,53 @@
+import oneflow as flow
+from onediff.infer_compiler import oneflow_compile, register
+from sd_webui_onediff_utils import (
+    CrossAttentionOflow,
+    GroupNorm32Oflow,
+    timestep_embedding,
+)
+from sgm.modules.attention import CrossAttention
+from sgm.modules.diffusionmodules.openaimodel import UNetModel
+from sgm.modules.diffusionmodules.util import GroupNorm32
+
+__all__ = ["compile_sgm_unet"]
+
+
+# https://github.com/Stability-AI/generative-models/blob/059d8e9cd9c55aea1ef2ece39abf605efb8b7cc9/sgm/modules/diffusionmodules/openaimodel.py#L816
+class UNetModelOflow(flow.nn.Module):
+    def forward(self, x, timesteps=None, context=None, y=None, **kwargs):
+        assert (y is not None) == (
+            self.num_classes is not None
+        ), "must specify y if and only if the model is class-conditional"
+        hs = []
+        t_emb = timestep_embedding(timesteps, self.model_channels).half()
+        emb = self.time_embed(t_emb)
+        x = x.half()
+        context = context.half() if context is not None else context
+        y = y.half() if y is not None else y
+        if self.num_classes is not None:
+            assert y.shape[0] == x.shape[0]
+            emb = emb + self.label_emb(y)
+        h = x
+        for module in self.input_blocks:
+            h = module(h, emb, context)
+            hs.append(h)
+        h = self.middle_block(h, emb, context)
+        for module in self.output_blocks:
+            h = flow.cat([h, hs.pop()], dim=1)
+            h = module(h, emb, context)
+        h = h.type(x.dtype)
+        return self.out(h)
+
+
+torch2oflow_class_map = {
+    CrossAttention: CrossAttentionOflow,
+    GroupNorm32: GroupNorm32Oflow,
+    UNetModel: UNetModelOflow,
+}
+register(package_names=["sgm"], torch2oflow_class_map=torch2oflow_class_map)
+
+
+def compile_sgm_unet(sd_model, *, use_graph=True, options={}):
+    unet_model = sd_model.model.diffusion_model
+    if isinstance(unet_model, UNetModel):
+        return oneflow_compile(unet_model, use_graph=use_graph, options=options)
--- onediff_sd_webui_extensions/scripts/onediff.py
+++ onediff_sd_webui_extensions/scripts/onediff.py
@@ -3,16 +3,11 @@ from modules import script_callbacks
 import modules.shared as shared
 from modules.processing import process_images
 
-import math
-import torch
-import oneflow as flow
-from einops import rearrange
-from oneflow import nn, einsum
-from sgm.modules.attention import default, CrossAttention
-from sgm.modules.diffusionmodules.util import GroupNorm32
-from omegaconf import OmegaConf, ListConfig
 from onediff.infer_compiler.transform.builtin_transform import torch2oflow
-from onediff.infer_compiler import oneflow_compile, register
+from omegaconf import OmegaConf, ListConfig
+
+from compile_sgm import compile_sgm_unet
+from compile_ldm import compile_ldm_unet
 
 
 @torch2oflow.register
@@ -22,123 +17,30 @@ def _(mod, verbose=False) -> ListConfig:
 
 
 """oneflow_compiled UNetModel"""
-_compiled = None
-
-
-
-# https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/modules/sd_hijack_optimizations.py#L142
-# https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/modules/sd_hijack_optimizations.py#L221
-class CrossAttentionOflow(nn.Module):
-    def __init__(
-        self,
-        query_dim,
-        context_dim=None,
-        heads=8,
-        dim_head=64,
-        dropout=0.0,
-        backend=None,
-    ):
-        super().__init__()
-        inner_dim = dim_head * heads
-        context_dim = default(context_dim, query_dim)
-
-        self.scale = dim_head**-0.5
-        self.heads = heads
-
-        self.to_q = nn.Linear(query_dim, inner_dim, bias=False)
-        self.to_k = nn.Linear(context_dim, inner_dim, bias=False)
-        self.to_v = nn.Linear(context_dim, inner_dim, bias=False)
-
-        self.to_out = nn.Sequential(
-            nn.Linear(inner_dim, query_dim), nn.Dropout(dropout)
-        )
-        self.backend = backend
-
-    def forward(
-        self,
-        x,
-        context=None,
-        mask=None,
-        additional_tokens=None,
-        n_times_crossframe_attn_in_self=0,
-    ):
-        h = self.heads
-
-        q_in = self.to_q(x)
-        context = default(context, x)
-
-        # context_k, context_v = hypernetwork.apply_hypernetworks(shared.loaded_hypernetworks, context)
-        context_k, context_v = context, context
-        k_in = self.to_k(context_k)
-        v_in = self.to_v(context_v)
-
-        out = flow._C.fused_multi_head_attention_inference_v2(
-            query=q_in,
-            query_layout="BM(HK)",
-            query_head_size=self.to_q.out_features//self.heads,
-            key=k_in,
-            key_layout="BM(HK)",
-            value=v_in,
-            value_layout="BM(HK)",
-            output_layout="BM(HK)",
-            causal=False,
-        )
-        return self.to_out(out)
-
-
-# https://github.com/Stability-AI/generative-models/blob/e5963321482a091a78375f3aeb2c3867562c913f/sgm/modules/diffusionmodules/wrappers.py#L24
-def forward_wrapper( self, x, t, c, **kwargs):
-    x = torch.cat((x, c.get("concat", torch.Tensor([]).type_as(x))), dim=1)
-    with torch.autocast("cuda", enabled=False):
-        with flow.autocast("cuda", enabled=False):
-            return self.diffusion_model(
-                x.half(),
-                timesteps=t.half(),
-                context=c.get("crossattn", None).half(),
-                y=c.get("vector", None).half(),
-                **kwargs,
-            )
-
-
-# https://github.com/Stability-AI/generative-models/blob/059d8e9cd9c55aea1ef2ece39abf605efb8b7cc9/sgm/modules/diffusionmodules/util.py#L274
-class GroupNorm32Oflow(nn.GroupNorm):
-    def forward(self, x):
-        # return super().forward(x.float()).type(x.dtype)
-        return super().forward(x).type(x.dtype)
-
-
-# https://github.com/Stability-AI/generative-models/blob/e5963321482a091a78375f3aeb2c3867562c913f/sgm/modules/diffusionmodules/openaimodel.py#L983-L984
-class TimeEmbedModule(nn.Module):
-    def __init__( self, time_embed):
-        super().__init__()
-        self._time_embed_module = time_embed
-
-    def forward(self, t_emb):
-        return self._time_embed_module(t_emb.half())
-
-
-torch2oflow_class_map = {
-    CrossAttention: CrossAttentionOflow,
-    GroupNorm32: GroupNorm32Oflow,
-}
-
-
-register(package_names=["sgm"], torch2oflow_class_map=torch2oflow_class_map)
+compiled_unet = None
 
 
 def compile(sd_model):
+    from ldm.modules.diffusionmodules.openaimodel import UNetModel as UNetModelLDM
+    from sgm.modules.diffusionmodules.openaimodel import UNetModel as UNetModelSGM
+
     unet_model = sd_model.model.diffusion_model
-    full_name = f"{unet_model.__module__}.{unet_model.__class__.__name__}"
-    if full_name != "sgm.modules.diffusionmodules.openaimodel.UNetModel":
-        return
-    global _compiled
-    _compiled = oneflow_compile(sd_model.model.diffusion_model, use_graph=True)
-    # add sgm package path to sys.path to avoid mock error
-    import sgm, sys
-    sys.path.append(sgm.__path__[0][:-4])
-    time_embed_wrapper = TimeEmbedModule(_compiled._deployable_module_model.oneflow_module.time_embed)
-    # https://github.com/Stability-AI/generative-models/blob/e5963321482a091a78375f3aeb2c3867562c913f/sgm/modules/diffusionmodules/openaimodel.py#L984
-    setattr(_compiled._deployable_module_model.oneflow_module, "time_embed", time_embed_wrapper)
+    global compiled_unet
+    if isinstance(unet_model, UNetModelLDM):
+        compiled_unet = compile_ldm_unet(sd_model)
+    elif isinstance(unet_model, UNetModelSGM):
+        compiled_unet = compile_sgm_unet(sd_model)
+
+
+def supplement_sys_path():
+    """add package path to sys.path to avoid mock error"""
+    import ldm, sgm, sys
+
+    sys_paths = set(sys.path)
+    new_paths = [sgm.__path__[0][:-4], ldm.__path__[0][:-4]]
+    for path in new_paths:
+        if path not in sys_paths:
+            sys.path.append(path)
 
 
 class Script(scripts.Script):
@@ -149,18 +51,14 @@ class Script(scripts.Script):
         return not is_img2img
 
     def run(self, p):
-        global _compiled
-        if _compiled is None:
-            compile(shared.sd_model)
+        global compiled_unet
+        if compiled_unet is None:
+            compiled_unet = compile(shared.sd_model)
         original = shared.sd_model.model.diffusion_model
-        from sgm.modules.diffusionmodules.wrappers import OpenAIWrapper
-        orig_forward = OpenAIWrapper.forward
-        if _compiled is not None:
-            shared.sd_model.model.diffusion_model = _compiled
-            setattr(OpenAIWrapper, "forward", forward_wrapper)
+        shared.sd_model.model.diffusion_model = compiled_unet
+        supplement_sys_path()
         proc = process_images(p)
         shared.sd_model.model.diffusion_model = original
-        setattr(OpenAIWrapper, "forward", orig_forward)
         return proc
 
 
