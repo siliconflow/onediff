9230260c93f3a3dc0b0dbd903112b45d952d020d
C <ichengzeyi@gmail.com>
2023-12-27 09:55:44 +0800

    Feature/svd support (#454)
    1. Add new example file `image_to_video_xt.py`
    2. Solve attention half accumulation overflow in SVD.
    3. Support compiling `unet`, `vae` and `image_encoder` in SVD.
    
    Requires:
    
    -
    https://github.com/siliconflow/cutlass-extension/pull/10#issue-2055294117
    - https://github.com/siliconflow/oneflow/pull/66#issue-2055297104
    
    Performance on RTX 4090:
    
    | ONEFLOW_ATTENTION_ALLOW_HALF_PRECISION_ACCUMULATION | MAX_M | Output |
    Duration |
    | --------------------------------------------------- | ------| ------ |
    -------- |
    | False | -1 | OK | 32.251s |
    | True | -1 | NaN | 29.089s |
    | True | 0 | OK | 30.947s |
    | True | 2304 | OK | 30.820s |

--- src/infer_compiler_registry/register_diffusers/__init__.py
+++ src/infer_compiler_registry/register_diffusers/__init__.py
@@ -37,36 +37,3 @@ if Transformer2DModelOflow is not None:
     torch2oflow_class_map.update({Transformer2DModel: Transformer2DModelOflow})
 
 register(torch2oflow_class_map=torch2oflow_class_map)
-
-
-from onediff.infer_compiler.transform import transform_mgr
-from onediff.infer_compiler.transform.builtin_transform import proxy_class
-
-
-_ONEFLOW_HAS_REGISTER_RELAXED_TYPE_API = False
-try:
-    from oneflow.framework.args_tree import register_relaxed_type
-
-    _ONEFLOW_HAS_REGISTER_RELAXED_TYPE_API = True
-except ImportError:
-    pass
-
-
-def register_args_tree_relaxed_types():
-    transformers_mocked = False
-    for pkg_name in transform_mgr.get_mocked_packages():
-        if "transformers" in pkg_name:
-            transformers_mocked = True
-            break
-
-    if _ONEFLOW_HAS_REGISTER_RELAXED_TYPE_API and transformers_mocked:
-        from transformers.modeling_outputs import BaseModelOutputWithPooling
-        from transformers.models.clip.modeling_clip import CLIPTextModelOutput
-
-        register_relaxed_type(proxy_class(BaseModelOutputWithPooling))
-        register_relaxed_type(proxy_class(CLIPTextModelOutput))
-    else:
-        pass
-
-
-register_args_tree_relaxed_types()
