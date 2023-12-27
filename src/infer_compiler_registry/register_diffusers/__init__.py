ac055b45fa704e33094eeb2eaa2811e5b51fce02
Li Xiang <54010254+lixiang007666@users.noreply.github.com>
2023-12-27 09:36:59 +0800

    Fix SVD VAE compile for diffuser (#461)
    This PR is done：
    
    - [x] Fix SVD VAE compile reshape for diffusers.
    
    打开 ONEFLOW_RUN_GRAPH_BY_VM 的情况下，编译 SVD 的 VAE。在**一次推理**中，diffusers 会有一些
    reshape op 涉及到动态维度，导致第一次执行 VAE 挂掉，这个 PR 处理了下。

--- src/infer_compiler_registry/register_diffusers/__init__.py
+++ src/infer_compiler_registry/register_diffusers/__init__.py
@@ -4,15 +4,34 @@ from onediff.infer_compiler.transform import register
 from diffusers.models.attention_processor import Attention, AttnProcessor2_0
 from diffusers.models.attention_processor import LoRAAttnProcessor2_0
 from diffusers.models.transformer_2d import Transformer2DModel
+from diffusers.models.transformer_temporal import TransformerSpatioTemporalModel
+from diffusers.models.resnet import SpatioTemporalResBlock
+from diffusers.models.attention import TemporalBasicTransformerBlock
+from diffusers.models.autoencoder_kl_temporal_decoder import TemporalDecoder
+
 from .attention_processor_oflow import Attention as AttentionOflow
 from .attention_processor_oflow import AttnProcessor as AttnProcessorOflow
 from .attention_processor_oflow import LoRAAttnProcessor2_0 as LoRAAttnProcessorOflow
 from .transformer_2d_oflow import Transformer2DModel as Transformer2DModelOflow
+from .transformer_spatial_oflow import (
+    TransformerSpatioTemporalModel as TransformerSpatioTemporalModelOflow,
+)
+from .transformer_spatial_oflow import (
+    SpatioTemporalResBlock as SpatioTemporalResBlockOflow,
+)
+from .transformer_spatial_oflow import (
+    TemporalBasicTransformerBlock as TemporalBasicTransformerBlockOflow,
+)
+from .transformer_spatial_oflow import TemporalDecoder as TemporalDecoderOflow
 
 torch2oflow_class_map = {
     Attention: AttentionOflow,
     AttnProcessor2_0: AttnProcessorOflow,
     LoRAAttnProcessor2_0: LoRAAttnProcessorOflow,
+    TransformerSpatioTemporalModel: TransformerSpatioTemporalModelOflow,
+    SpatioTemporalResBlock: SpatioTemporalResBlockOflow,
+    TemporalBasicTransformerBlock: TemporalBasicTransformerBlockOflow,
+    TemporalDecoder: TemporalDecoderOflow,
 }
 if Transformer2DModelOflow is not None:
     torch2oflow_class_map.update({Transformer2DModel: Transformer2DModelOflow})
