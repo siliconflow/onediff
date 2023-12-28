3437576cdd02277dde0e37156156be370ab8d421
lixiang007666 <88304454@qq.com>
2023-12-28 10:48:47 +0800

    Add version control

--- src/infer_compiler_registry/register_diffusers/__init__.py
+++ src/infer_compiler_registry/register_diffusers/__init__.py
@@ -1,12 +1,20 @@
 from onediff.infer_compiler.transform import register
 
+from packaging import version
+import importlib.metadata
+
+diffusers_version = version.parse(importlib.metadata.version("diffusers"))
+
 # register(package_names=["diffusers"])
 from diffusers.models.attention_processor import Attention, AttnProcessor2_0
 from diffusers.models.attention_processor import LoRAAttnProcessor2_0
 from diffusers.models.transformer_2d import Transformer2DModel
 from diffusers.models.resnet import SpatioTemporalResBlock
 from diffusers.models.attention import TemporalBasicTransformerBlock
-from diffusers.models.autoencoders.autoencoder_kl_temporal_decoder import TemporalDecoder
+if diffusers_version > version.parse("0.24.99"):
+    from diffusers.models.autoencoders.autoencoder_kl_temporal_decoder import TemporalDecoder
+else:
+    from diffusers.models.autoencoder_kl_temporal_decoder import TemporalDecoder
 
 from .attention_processor_oflow import Attention as AttentionOflow
 from .attention_processor_oflow import AttnProcessor as AttnProcessorOflow
