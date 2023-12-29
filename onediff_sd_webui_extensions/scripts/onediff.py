e8cb473aadc28724f6d3731f8b6630d90e9779ae
ccsuu <ccsuwen@gmail.com>
2023-12-29 09:09:03 +0000

    fix varify_can_use_quantization gr.HTML

--- onediff_sd_webui_extensions/scripts/onediff.py
+++ onediff_sd_webui_extensions/scripts/onediff.py
@@ -68,27 +68,28 @@ class Script(scripts.Script):
         """
         if not varify_can_use_quantization():
             ret = gr.HTML(
-                """
-                <div style="padding: 20px; border: 1px solid #e0e0e0; border-radius: 5px; background-color: #f9f9f9;">
-                    <div style="font-size: 18px; font-weight: bold; margin-bottom: 15px; color: #d9534f;">
-                        Error Message
+                    """
+                    <div style="padding: 20px; border: 1px solid #e0e0e0; border-radius: 5px; background-color: #f9f9f9;">
+                        <div style="font-size: 18px; font-weight: bold; margin-bottom: 15px; color: #31708f;">
+                            Hints Message
+                        </div>
+                        <div style="padding: 10px; border: 1px solid #31708f; border-radius: 5px; background-color: #f9f9f9;">
+                            Hints: Enterprise function is not supported on your system.
+                        </div>
+                        <p style="margin-top: 15px;">
+                            If you need Enterprise Level Support for your system or business, please send an email to 
+                            <a href="mailto:business@siliconflow.com" style="color: #31708f; text-decoration: none;">business@siliconflow.com</a>.
+                            <br>
+                            Tell us about your use case, deployment scale, and requirements.
+                        </p>
+                        <p>
+                            <strong>GitHub Issue:</strong>
+                            <a href="https://github.com/siliconflow/onediff/issues" style="color: #31708f; text-decoration: none;">https://github.com/siliconflow/onediff/issues</a>
+                        </p>
                     </div>
-                    <div class='error-message' style="padding: 10px; border: 1px solid #d9534f; border-radius: 5px; background-color: #f2dede;">
-                        Error: Enterprise function is not supported on your system.
-                    </div>
-                    <p style="margin-top: 15px;">
-                        If you need Enterprise Level Support for your system or business, please send an email to 
-                        <a href="mailto:business@siliconflow.com" style="color: #31708f; text-decoration: none;">business@siliconflow.com</a>.
-                        <br>
-                        Tell us about your use case, deployment scale, and requirements.
-                    </p>
-                    <p>
-                        <strong>GitHub Issue:</strong>
-                        <a href="https://github.com/siliconflow/onediff/issues" style="color: #31708f; text-decoration: none;">https://github.com/siliconflow/onediff/issues</a>
-                    </p>
-                </div>
-            """
-            )
+                    """
+                )
+
         else:
             ret = gr.components.Checkbox(label="Model Quantization(int8) Speed Up")
         return [ret]
