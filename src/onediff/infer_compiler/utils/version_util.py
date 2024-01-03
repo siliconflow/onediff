e40dc1cb796cd135613e5bd3701cf31b4194903f
FengWen <ccsuwen@gmail.com>
2024-01-03 06:16:30 +0000

    update_version_utils.py

--- src/onediff/infer_compiler/utils/version_util.py
+++ src/onediff/infer_compiler/utils/version_util.py
@@ -1,8 +1,8 @@
-from importlib_metadata import version
 from .log_utils import logger
 
+
 def get_support_message():
-    recipient_email = "caishenghang@oneflow.org"
+    recipient_email = "business@siliconflow.com"
 
     message = f"""\033[91m Advanced features cannot be used !!! \033[0m
 If you need unrestricted multiple resolution, quantization support or any other more advanced features, please send an email to \033[91m{recipient_email}\033[0m and tell us about your use case, deployment scale and requirements.
@@ -10,24 +10,29 @@ If you need unrestricted multiple resolution, quantization support or any other 
     return message
 
 
-def is_quantization_enabled():
-    import oneflow
-
-    if version("oneflow") < "0.9.1":
-        RuntimeError(
-            "onediff_comfy_nodes requires oneflow>=0.9.1 to run.", get_support_message()
-        )
-        return False
+def is_oneflow_pro() -> bool:
     try:
-        import diffusers_quant
-    except ImportError as e:
-        logger.warning(
-            f"Failed to import diffusers_quant, Error message: {e}, {get_support_message()}"
-        )
+        import oneflow.sysconfig as sysconfig
+
+        return sysconfig.with_enterprise()
+    except Exception as e:
+        logger.warning(f"Unable to determine if OneFlow is Pro or not. {e}")
+        logger.warning(get_support_message())
         return False
-    return hasattr(oneflow._C, "dynamic_quantization")
 
 
 def is_community_version():
-    is_community = not is_quantization_enabled()
-    return is_community
+    return not is_oneflow_pro()
+
+
+def is_quantization_enabled() -> bool:
+    if not is_oneflow_pro():
+        return False
+    try:
+        import diffusers_quant
+
+        return True
+    except Exception as e:
+        logger.warning(f"Quantization is not enabled. {e}")
+        logger.warning(get_support_message())
+        return False
