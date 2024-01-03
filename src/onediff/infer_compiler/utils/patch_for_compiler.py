79a1127476810f1cc7f95418615b961cdd7c5f9e
ccsuu <ccsuwen@gmail.com>
2024-01-03 12:09:49 +0000

    tmp fix

--- src/onediff/infer_compiler/utils/patch_for_compiler.py
+++ src/onediff/infer_compiler/utils/patch_for_compiler.py
@@ -98,6 +98,14 @@ class FakeCuda:
         )
         return out
 
+# TODO(): remove this patch
+org_to = flow._C.to 
+def to(*args, **kwargs):
+    kwargs.pop("non_blocking", None)
+    return org_to(*args, **kwargs)
+flow._C.to = to
+
+
 
 flow.cuda.current_device = FakeCuda.current_device
 flow.cuda.mem_get_info = FakeCuda.mem_get_info
