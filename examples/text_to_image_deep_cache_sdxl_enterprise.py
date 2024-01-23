09ef175940eef06dad9d8f1812b0cf1f7145ba47
Yao Chi <later@usopp.net>
2024-01-23 06:23:39 +0000

    fix typos

--- examples/text_to_image_deep_cache_sdxl_enterprise.py
+++ examples/text_to_image_deep_cache_sdxl_enterprise.py
@@ -28,12 +28,12 @@ def parse_args():
     parser.add_argument("--steps", type=int, default=30)
     parser.add_argument("--bits", type=int, default=8)
     parser.add_argument(
-        "--complie",
+        "--compile",
         default=True,
         type=(lambda x: str(x).lower() in ["true", "1", "yes"]),
     )
     parser.add_argument(
-        "--complie_text_encoder",
+        "--compile_text_encoder",
         default=False,
         type=(lambda x: str(x).lower() in ["true", "1", "yes"]),
         help=(
@@ -63,8 +63,8 @@ assert os.path.isfile(
 ), f"calibrate_info.txt is required in args.model ({args.model})"
 
 assert (
-    args.complie
-), "Onediff enterprise model can not be executed in pytorch environment. Please set args.complie to 1!"
+    args.compile
+), "Onediff enterprise model can not be executed in pytorch environment. Please set args.compile to 1!"
 
 from diffusers_extensions.deep_cache import StableDiffusionXLPipeline
 import onediff_quant
@@ -115,13 +115,13 @@ for sub_module_name, sub_calibrate_info in calibrate_info.items():
         maybe_allow_in_graph,
     )
 
-if args.complie_text_encoder:
+if args.compile_text_encoder:
     if pipe.text_encoder is not None:
         pipe.text_encoder = oneflow_compile(pipe.text_encoder, use_graph=args.graph)
     if pipe.text_encoder_2 is not None:
         pipe.text_encoder_2 = oneflow_compile(pipe.text_encoder_2, use_graph=args.graph)
 
-if args.complie:
+if args.compile:
     if pipe.text_encoder is not None:
         pipe.text_encoder = oneflow_compile(pipe.text_encoder, use_graph=args.graph)
     if pipe.text_encoder_2 is not None:
