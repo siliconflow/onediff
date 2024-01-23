589c8ac993297bac77ad4a4821c78475c08fa3d8
clackhan <han_binbin@163.com>
2024-01-23 11:09:01 +0000

    refine

--- examples/text_to_image_sdxl_enterprise.py
+++ examples/text_to_image_sdxl_enterprise.py
@@ -47,9 +47,6 @@ def parse_args():
     )
     parser.add_argument("--seed", type=int, default=1)
     parser.add_argument("--warmup", type=int, default=1)
-    parser.add_argument("--cache_interval", type=int, default=3)
-    parser.add_argument("--cache_layer_id", type=int, default=0)
-    parser.add_argument("--cache_block_id", type=int, default=0)
 
     args = parser.parse_args()
     return args
@@ -118,10 +115,6 @@ if args.compile_text_encoder:
         pipe.text_encoder_2 = oneflow_compile(pipe.text_encoder_2, use_graph=args.graph)
 
 if args.compile:
-    if pipe.text_encoder is not None:
-        pipe.text_encoder = oneflow_compile(pipe.text_encoder, use_graph=args.graph)
-    if pipe.text_encoder_2 is not None:
-        pipe.text_encoder_2 = oneflow_compile(pipe.text_encoder_2, use_graph=args.graph)
     pipe.unet = oneflow_compile(pipe.unet, use_graph=args.graph)
     pipe.vae.decoder = oneflow_compile(pipe.vae.decoder, use_graph=args.graph)
 
