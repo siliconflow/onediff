e777af60c71e9b7a1974bda74a21638969438fb1
FengWen <109639975+ccssu@users.noreply.github.com>
2024-01-24 16:16:16 +0800

    fix ControlLora usage (#565)
    - 修复 controllora ，修改 length参数触发编译。
    - 支持 OneDiffControlNetLoader 自动缓存和加载图。
    - doc:
    https://github.com/siliconflow/onediff/tree/fix_ControlLora_use/onediff_comfy_nodes/workflows/ControlNet#basic-usage

--- onediff_comfy_nodes/utils/model_patcher.py
+++ onediff_comfy_nodes/utils/model_patcher.py
@@ -496,6 +496,7 @@ class OneFlowDeepCacheSpeedUpModelPatcher(OneFlowSpeedUpModelPatcher):
         weight_inplace_update=False,
         *,
         use_graph=None,
+        gen_compile_options=None,
     ):
         from onediff.infer_compiler import oneflow_compile
         from onediff.infer_compiler.with_oneflow_compile import DeployableModule
@@ -514,15 +515,19 @@ class OneFlowDeepCacheSpeedUpModelPatcher(OneFlowSpeedUpModelPatcher):
         self.fast_deep_cache_unet = FastDeepCacheUNet(
             self.model.diffusion_model, cache_layer_id, cache_block_id
         )
-
         if use_graph:
+            gen_compile_options = gen_compile_options or (lambda x: {})
+            compile_options = gen_compile_options(self.deep_cache_unet)
             self.deep_cache_unet = oneflow_compile(
                 self.deep_cache_unet,
                 use_graph=use_graph,
+                options=compile_options,
             )
+            compile_options = gen_compile_options(self.fast_deep_cache_unet)
             self.fast_deep_cache_unet = oneflow_compile(
                 self.fast_deep_cache_unet,
                 use_graph=use_graph,
+                options=compile_options,
             )
             self.model._register_state_dict_hook(state_dict_hook)
 
