5c38994b0917a092055226e537eefeb3f6270e41
FengWen <ccsuwen@gmail.com>
2024-01-28 20:29:51 +0800

    d2bf367 (HEAD -> main, origin/main, origin/HEAD) HEAD@{0}: clone: from github.com:Kosinkadink/ComfyUI-AnimateDiff-Evolved.git

--- onediff_comfy_nodes/modules/hijack_animatediff/__init__.py
+++ onediff_comfy_nodes/modules/hijack_animatediff/__init__.py
@@ -1,3 +1,4 @@
+# d2bf367 (HEAD -> main, origin/main, origin/HEAD) HEAD@{0}: clone: from github.com:Kosinkadink/ComfyUI-AnimateDiff-Evolved.git
 from .sampling import *
 from .motion_utils import *
 from ._config import animatediff_hijacker
