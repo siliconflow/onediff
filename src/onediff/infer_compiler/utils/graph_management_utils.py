93cd013efd6ebfbf9ec0ef9645c1bb2a88441ad7
ccsuu <ccsuwen@gmail.com>
2024-01-29 11:24:57 +0000

    add is_first_load

--- src/onediff/infer_compiler/utils/graph_management_utils.py
+++ src/onediff/infer_compiler/utils/graph_management_utils.py
@@ -15,43 +15,41 @@ def calculate_model_hash(model):
     return hashlib.sha256(f"{model}".encode("utf-8")).hexdigest()
 
 
+@cost_time(debug=transform_mgr.debug_mode, message="generate graph file name")
+def generate_graph_file_name(file_path, deployable_module, args, kwargs):
+    if isinstance(file_path, Path):
+        file_path = str(file_path)
+
+    if file_path.endswith(".graph"):
+        file_path = file_path[:-6]
+
+    args_tree = ArgsTree((args, kwargs), False, tensor_type=torch.Tensor)
+    count = len([v for v in args_tree.iter_nodes() if isinstance(v, flow.Tensor)])
+
+    model = deployable_module._deployable_module_model.oneflow_module
+
+    cache_key = calculate_model_hash(model) + "_" + flow.__version__
+    return f"{file_path}_{count}_{cache_key}.graph"
+
+
 def graph_file_management(func):
     @wraps(func)
     def wrapper(self: "DeployableModule", *args, **kwargs):
         compile_options = getattr(self, "_deployable_module_options", {})
-        graph_file = compile_options.get("graph_file")
+        graph_file = compile_options.get("graph_file", None)
+        is_first_load = (
+            getattr(self, "_load_graph_first_run", True) and graph_file is not None
+        )
 
-        def generate_graph_file_name(file_path):
-            if isinstance(file_path, Path):
-                file_path = str(file_path)
-
-            if file_path.endswith(".graph"):
-                file_path = file_path[:-6]
-
-            with cost_time(
-                debug=transform_mgr.debug_mode, message="calculate model input count"
-            ):
-                args_tree = ArgsTree((args, kwargs), False, tensor_type=torch.Tensor)
-                count = len(
-                    [v for v in args_tree.iter_nodes() if isinstance(v, flow.Tensor)]
-                )
-
-            with cost_time(debug=transform_mgr.debug_mode, message="get model"):
-                model = self._deployable_module_model.oneflow_module
-
-            with cost_time(
-                debug=transform_mgr.debug_mode,
-                message="calculate model hash for cache key",
-            ):
-                cache_key = calculate_model_hash(model) + "_" + flow.__version__
-            return f"{file_path}_{count}_{cache_key}.graph"
-
-        if graph_file:
-            graph_file = generate_graph_file_name(graph_file)
+        if is_first_load:
+            graph_file = generate_graph_file_name(
+                graph_file, self, args=args, kwargs=kwargs
+            )
+            setattr(self, "_load_graph_first_run", False)
 
         def handle_graph_loading():
-            nonlocal graph_file, compile_options
-            if graph_file is None:
+            nonlocal graph_file, compile_options, is_first_load
+            if not is_first_load:
                 return
 
             if not os.path.exists(graph_file):
@@ -62,12 +60,11 @@ def graph_file_management(func):
                 graph_device = compile_options.get("graph_file_device")
                 self.load_graph(graph_file, torch2oflow(graph_device))
                 logger.info(f"Loaded graph file: {graph_file}")
-                compile_options["graph_file"] = None
-                graph_file = None
+                is_first_load = False
 
         def handle_graph_saving():
-            nonlocal graph_file, compile_options
-            if graph_file is None:
+            nonlocal graph_file, compile_options, is_first_load
+            if not is_first_load:
                 return
 
             try:
@@ -80,9 +77,6 @@ def graph_file_management(func):
 
             except Exception as e:
                 logger.error(f"Failed to save graph file: {graph_file}! {e}")
-
-            finally:
-                compile_options["graph_file"] = None
 
         handle_graph_loading()
         ret = func(self, *args, **kwargs)
