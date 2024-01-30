d94356a5a06d7f5e22f0a2ff8edb499f7d1bf30a
ccsuu <ccsuwen@gmail.com>
2024-01-30 01:49:55 +0000

    Fix Check Failed Error

--- src/onediff/infer_compiler/utils/args_tree_util.py
+++ src/onediff/infer_compiler/utils/args_tree_util.py
@@ -1,6 +1,7 @@
 import torch
 import oneflow as flow
 from oneflow.framework.args_tree import ArgsTree
+from .log_utils import logger
 
 
 def input_output_processor(func):
@@ -13,10 +14,13 @@ def input_output_processor(func):
                 return value
 
         args_tree = ArgsTree((args, kwargs), False, tensor_type=torch.Tensor)
+        input_count = len(
+            [v for v in args_tree.iter_nodes() if isinstance(v, torch.Tensor)]
+        )
         out = args_tree.map_leaf(input_fn)
         mapped_args = out[0]
         mapped_kwargs = out[1]
-        return mapped_args, mapped_kwargs
+        return mapped_args, mapped_kwargs, input_count
 
     def process_output(output):
         def output_fn(value):
@@ -30,7 +34,21 @@ def input_output_processor(func):
         return out[0]
 
     def wrapper(cls, *args, **kwargs):
-        mapped_args, mapped_kwargs = process_input(*args, **kwargs)
+        mapped_args, mapped_kwargs, input_count = process_input(*args, **kwargs)
+        if cls._deployable_module_use_graph:
+            count = getattr(cls, "_deployable_module_input_count", input_count)
+            if count != input_count:
+                logger.warning(
+                    f"{type(cls._deployable_module_model.oneflow_module)} input count changed, will reload graph. "
+                    f"When setting the strength parameter in the 'Apply ControlNet' node, please exercise caution. "
+                    f"Please check and avoid toggling the strength parameter between values greater than 0 and 0, as it may lead to unnecessary graph reloading."
+                )
+
+                del cls._deployable_module_model.oneflow_module
+                cls._deployable_module_dpl_graph = None
+                setattr(cls, "_load_graph_first_run", True)
+
+            setattr(cls, "_deployable_module_input_count", input_count)
         output = func(cls, *mapped_args, **mapped_kwargs)
         return process_output(output)
 
