c52849dea57afe39a0c39ccdfe2e16ff1d160345
chengzeyi <ichengzeyi@gmail.com>
2024-01-30 10:26:26 +0800

    fix

--- onediff_diffusers_extensions/diffusers_extensions/__init__.py
+++ onediff_diffusers_extensions/diffusers_extensions/__init__.py
@@ -1,4 +1,4 @@
-from .compilers.diffusion_pipeline_compiler import compile_pipe
 from onediff.infer_compiler.oneflow_compiler_config import oneflow_compiler_config as compiler_config
+from .compilers.diffusion_pipeline_compiler import compile_pipe
 
 __all__ = ['compile_pipe', 'compiler_config']
