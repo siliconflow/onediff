c98970f4474aa0d12cb624428e7a193879a934be
FengWen <ccsuwen@gmail.com>
2024-01-30 07:14:13 +0000

    refine

--- tests/comfyui/conftest.py
+++ tests/comfyui/conftest.py
@@ -3,6 +3,7 @@ import yaml
 import argparse
 from pathlib import Path
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -18,13 +19,14 @@ def parse_args():
 
 def create_symlink(source, destination):
     try:
-        print(f'Creating symlink: {destination} -> {source}')
+        print(f"Creating symlink: {destination} -> {source}")
         if os.path.exists(destination):
             os.remove(destination)
         os.symlink(source, destination)
     except Exception as e:
         # warn if symlink creation fails
-        print(f'Warning: {e}')
+        print(f"Warning: {e}")
+
 
 def process_datasets(data, current_path):
     if isinstance(data, dict):
@@ -38,19 +40,21 @@ def process_datasets(data, current_path):
         current_path.mkdir(parents=True, exist_ok=True)
         create_symlink(data, current_path / base_name)
 
+
 def main():
     args = parse_args()
     file_path = args.config_file
 
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         config_data = yaml.safe_load(file)
 
     comfyui_root = config_data.pop("COMFYUI_ROOT", None)
 
     if not comfyui_root or not os.path.exists(comfyui_root):
-        raise ValueError("COMFYUI_ROOT is not set or does not exist")   
+        raise ValueError("COMFYUI_ROOT is not set or does not exist")
 
     process_datasets(config_data, Path(comfyui_root))
+
 
 if __name__ == "__main__":
     main()
