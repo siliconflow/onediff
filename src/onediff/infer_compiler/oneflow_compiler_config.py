214266e268b51d4d556689a8d24dfcd5487633a9
chengzeyi <ichengzeyi@gmail.com>
2024-02-01 21:20:11 +0800

    set ONEFLOW_KERNEL_GLU_ENABLE_Y_GEMM_IMPL=0 by default and add attention_allow_quantization option

--- src/onediff/infer_compiler/oneflow_compiler_config.py
+++ src/onediff/infer_compiler/oneflow_compiler_config.py
@@ -29,13 +29,14 @@ def init_default_env():
     os.environ.setdefault("ONEFLOW_KERNEL_CONV_ENABLE_CUTLASS_IMPL", "1")
     os.environ.setdefault("ONEFLOW_KERNEL_GEMM_ENABLE_CUTLASS_IMPL", "1")
     # os.environ.setdefault("ONEFLOW_KERNEL_GLU_ENABLE_DUAL_GEMM_IMPL", "0")
-    # os.environ.setdefault("ONEFLOW_KERNEL_GLU_ENABLE_Y_GEMM_IMPL", "0")
+    os.environ.setdefault("ONEFLOW_KERNEL_GLU_ENABLE_Y_GEMM_IMPL", "0")
 
     os.environ.setdefault("ONEFLOW_CONV_ALLOW_HALF_PRECISION_ACCUMULATION", "1")
     os.environ.setdefault("ONEFLOW_MATMUL_ALLOW_HALF_PRECISION_ACCUMULATION", "1")
     os.environ.setdefault("ONEFLOW_LINEAR_EMBEDDING_SKIP_INIT", "1")
     # os.environ.setdefault("ONEFLOW_ATTENTION_ALLOW_HALF_PRECISION_ACCUMULATION", "1")
     # os.environ.setdefault("ONEFLOW_ATTENTION_ALLOW_HALF_PRECISION_SCORE_ACCUMULATION_MAX_M", "-1")
+    os.environ.setdefault("ONEFLOW_ATTENTION_ALLOW_QUANTIZATION", "0")
 
     os.environ.setdefault("ONEFLOW_MLIR_GROUP_MATMUL_QUANT", "1")
 
@@ -73,6 +74,7 @@ class OneFlowCompilerConfig:
     linear_embedding_skip_init: Optional[bool] = None
     attention_allow_half_precision_accumulation: Optional[bool] = None
     attention_allow_half_precision_score_accumulation_max_m: Optional[int] = None
+    attention_allow_quantization: Optional[bool] = None
 
     attr2env_var = {
         "run_graph_by_vm": "ONEFLOW_RUN_GRAPH_BY_VM",
