88a788bad25c416e13d7003b94a722e44869b8be
FengWen <109639975+ccssu@users.noreply.github.com>
2024-03-02 09:10:02 +0800

    fix_svd_resolution_error (#690)
    修复： https://github.com/siliconflow/onediff/issues/683
    - [ ] 将 svd 加入ci
    
    ---------
    
    Co-authored-by: Xiaoyu Xu <xiaoyulink@gmail.com>

--- onediff_comfy_nodes/infer_compiler_registry/register_comfy/util.py
+++ onediff_comfy_nodes/infer_compiler_registry/register_comfy/util.py
@@ -1,3 +1,4 @@
+""" ComfyUI/comfy/ldm/modules/diffusionmodules/util.py """
 import oneflow as torch
 import oneflow.nn as nn
 import oneflow.nn.functional as F
@@ -8,57 +9,34 @@ from einops import rearrange
 class AlphaBlender(nn.Module):
     strategies = ["learned", "fixed", "learned_with_images"]
 
-    def __init__(
-        self,
-        alpha: float,
-        merge_strategy: str = "learned_with_images",
-        rearrange_pattern: str = "b t -> (b t) 1 1",
-    ):
-        super().__init__()
-        self.merge_strategy = merge_strategy
-        self.rearrange_pattern = rearrange_pattern
-
-        assert (
-            merge_strategy in self.strategies
-        ), f"merge_strategy needs to be in {self.strategies}"
-
-        if self.merge_strategy == "fixed":
-            self.register_buffer("mix_factor", torch.Tensor([alpha]))
-        elif (
-            self.merge_strategy == "learned"
-            or self.merge_strategy == "learned_with_images"
-        ):
-            self.register_parameter(
-                "mix_factor", torch.nn.Parameter(torch.Tensor([alpha]))
-            )
-        else:
-            raise ValueError(f"unknown merge strategy {self.merge_strategy}")
-
-    def get_alpha(self, image_only_indicator: torch.Tensor) -> torch.Tensor:
+    def get_alpha(self, image_only_indicator: torch.Tensor, device) -> torch.Tensor:
         # skip_time_mix = rearrange(repeat(skip_time_mix, 'b -> (b t) () () ()', t=t), '(b t) 1 ... -> b 1 t ...', t=t)
         if self.merge_strategy == "fixed":
             # make shape compatible
             # alpha = repeat(self.mix_factor, '1 -> b () t  () ()', t=t, b=bs)
-            alpha = self.mix_factor.to(image_only_indicator.device)
+            alpha = self.mix_factor.to(device)
         elif self.merge_strategy == "learned":
-            alpha = torch.sigmoid(self.mix_factor.to(image_only_indicator.device))
+            alpha = torch.sigmoid(self.mix_factor.to(device))
             # make shape compatible
             # alpha = repeat(alpha, '1 -> s () ()', s = t * bs)
         elif self.merge_strategy == "learned_with_images":
-            assert image_only_indicator is not None, "need image_only_indicator ..."
-            # alpha = torch.where(
-            #     image_only_indicator.bool(),
-            #     torch.ones(1, 1, device=image_only_indicator.device),
-            #     rearrange(torch.sigmoid(self.mix_factor.to(image_only_indicator.device)), "... -> ... 1"),
-            # )
-            # Rewrite for onediff SVD dynamic shape
-            alpha = torch.where(
-                image_only_indicator.bool(),
-                torch.ones(1, 1, device=image_only_indicator.device),
-                torch.sigmoid(
-                    self.mix_factor.to(image_only_indicator.device)
-                ).unsqueeze(-1),
-            )
+            # assert image_only_indicator is not None, "need image_only_indicator ..."
+            if image_only_indicator is None:
+                alpha = rearrange(
+                    torch.sigmoid(self.mix_factor.to(device)), "... -> ... 1"
+                )
+            else:
+                # alpha = torch.where(
+                #     image_only_indicator.bool(),
+                #     torch.ones(1, 1, device=image_only_indicator.device),
+                #     rearrange(torch.sigmoid(self.mix_factor.to(image_only_indicator.device)), "... -> ... 1"),
+                # )
+                # Rewrite for onediff SVD dynamic shape
+                alpha = torch.where(
+                    image_only_indicator.bool(),
+                    torch.ones(1, 1, device=device),
+                    torch.sigmoid(self.mix_factor.to(device)).unsqueeze(-1),
+                )
             # alpha = rearrange(alpha, self.rearrange_pattern)
             # Rewrite for onediff SVD dynamic shape, only VideoResBlock, rearrange_pattern="b t -> b 1 t 1 1",
             if self.rearrange_pattern == "b t -> b 1 t 1 1":
@@ -76,7 +54,7 @@ class AlphaBlender(nn.Module):
     def forward(
         self, x_spatial, x_temporal, image_only_indicator=None,
     ) -> torch.Tensor:
-        alpha = self.get_alpha(image_only_indicator)
+        alpha = self.get_alpha(image_only_indicator, x_spatial.device)
         x = (
             alpha.to(x_spatial.dtype) * x_spatial
             + (1.0 - alpha).to(x_spatial.dtype) * x_temporal
