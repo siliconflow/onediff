b84264d0652cf1f6eb4689172e315a22e2c425ec
hjchen2 <chenhoujiangcug@gmail.com>
2024-03-05 22:55:32 +0800

    quantize pipeline

--- src/onediff/quantization/quantize_pipeline.py
+++ src/onediff/quantization/quantize_pipeline.py
@@ -1,10 +1,27 @@
 import os
 from typing import Any, List, Optional, Union
+from functools import partial
 
+from onediff_quant import quantize_pipeline, save_quantized
 from .quantize_utils import setup_onediff_quant, load_calibration_and_quantize_pipeline
 
 
 class QuantPipeline:
+    @classmethod
+    def from_quantized(
+        self,
+        cls,
+        pretrained_model_name_or_path: Optional[Union[str, os.PathLike]],
+        *args,
+        **kwargs
+    ):
+        setup_onediff_quant()
+        pipe = cls.from_pretrained(pretrained_model_name_or_path, *args, **kwargs)
+        load_calibration_and_quantize_pipeline(
+            os.path.join(str(pretrained_model_name_or_path), "calibrate_info.txt"), pipe
+        )
+        return pipe
+
     @classmethod
     def from_pretrained(
         self,
@@ -13,12 +30,7 @@ class QuantPipeline:
         *args,
         **kwargs
     ):
-        setup_onediff_quant()
-
         pipe = cls.from_pretrained(pretrained_model_name_or_path, *args, **kwargs)
-
-        load_calibration_and_quantize_pipeline(
-            os.path.join(str(pretrained_model_name_or_path), "calibrate_info.txt"), pipe
-        )
-
+        pipe.quantize = partial(quantize_pipeline, pipe)
+        pipe.save_quantized = partial(save_quantized, pipe)
         return pipe
