53470e686d1b2411a8f210e8641200608e3bf671
Yao Chi <later@usopp.net>
2024-03-05 15:42:53 +0800

    init dev

--- src/onediff/infer_compiler/with_oneflow_compile.py
+++ src/onediff/infer_compiler/with_oneflow_compile.py
@@ -343,40 +343,14 @@ class OneflowGraph(flow.nn.Graph):
     @cost_cnt(transform_mgr.debug_mode)
     def load_graph(self, file_path, device=None, run_warmup=True):
         state_dict = flow.load(file_path)
+        self.graph_state_dict = state_dict # for used for OneflowGraph.save_graph
         if device is not None:
             state_dict = flow.nn.Graph.runtime_state_dict_to(state_dict, device)
         self.load_runtime_state_dict(state_dict, warmup_with_run=run_warmup)
 
     @cost_cnt(transform_mgr.debug_mode)
     def save_graph(self, file_path):
-        state_dict = self.runtime_state_dict()
-
-        import oneflow.framework.args_tree as args_tree
-
-        def disabled_dataclass(value):
-            return False
-
-        original_is_dataclass = args_tree._is_dataclass
-        args_tree._is_dataclass = disabled_dataclass
-
-        import dataclasses
-
-        def reverse_dataclass(value):
-            if dataclasses.is_dataclass(value):
-                return reverse_proxy_class(type(value))(**value)
-            else:
-                return value
-
-        for name, rsd in state_dict.items():
-            output = state_dict[name]["outputs_original"]
-            out_tree = args_tree.ArgsTree((output, None), False)
-            # dataclass type needs to be reversed to torch type to avoid saving error.
-            out = out_tree.map_leaf(reverse_dataclass)
-            state_dict[name]["outputs_original"] = out[0]
-
-        args_tree._is_dataclass = original_is_dataclass
-
-        flow.save(state_dict, file_path)
+        flow.save(self.graph_state_dict, file_path)
 
 
 def get_oneflow_graph(model, size=9, dynamic_graph=True):
