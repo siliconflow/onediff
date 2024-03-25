4a866a52ec2cd6d0d95a7f113281d178f5cda4f4
binbinHan <han_binbin@163.com>
2024-03-25 16:37:58 +0800

    fix_multi_resulotion_error (#754)
    fix svd multi resolution error in
    https://github.com/siliconflow/onediff/issues/753

--- src/infer_compiler_registry/register_diffusers/__init__.py
+++ src/infer_compiler_registry/register_diffusers/__init__.py
@@ -28,14 +28,15 @@ else:
     from diffusers.models.resnet import Upsample2D
 if diffusers_version >= version.parse("0.24.00"):
     from diffusers.models.resnet import SpatioTemporalResBlock
-    from diffusers.models.transformer_temporal import TransformerSpatioTemporalModel
     from diffusers.models.attention import TemporalBasicTransformerBlock
 
     if diffusers_version >= version.parse("0.26.00"):
         from diffusers.models.unets.unet_spatio_temporal_condition import (
             UNetSpatioTemporalConditionModel,
         )
+        from diffusers.models.transformers.transformer_temporal import TransformerSpatioTemporalModel
     else:
+        from diffusers.models.transformer_temporal import TransformerSpatioTemporalModel
         from diffusers.models.unet_spatio_temporal_condition import (
             UNetSpatioTemporalConditionModel,
         )
@@ -46,6 +47,18 @@ if diffusers_version >= version.parse("0.24.00"):
         )
     else:
         from diffusers.models.autoencoder_kl_temporal_decoder import TemporalDecoder
+    
+    from .spatio_temporal_oflow import SpatioTemporalResBlock as SpatioTemporalResBlockOflow
+    from .spatio_temporal_oflow import TemporalDecoder as TemporalDecoderOflow
+    from .spatio_temporal_oflow import (
+        TransformerSpatioTemporalModel as TransformerSpatioTemporalModelOflow,
+    )
+    from .spatio_temporal_oflow import (
+        TemporalBasicTransformerBlock as TemporalBasicTransformerBlockOflow,
+    )
+    from .spatio_temporal_oflow import (
+        UNetSpatioTemporalConditionModel as UNetSpatioTemporalConditionModelOflow,
+    )
 
 from .attention_processor_oflow import Attention as AttentionOflow
 from .attention_processor_oflow import AttnProcessor as AttnProcessorOflow
@@ -56,17 +69,6 @@ from .unet_2d_blocks_oflow import CrossAttnUpBlock2D as CrossAttnUpBlock2DOflow
 from .unet_2d_blocks_oflow import UpBlock2D as UpBlock2DOflow
 from .resnet_oflow import Upsample2D as Upsample2DOflow
 from .transformer_2d_oflow import Transformer2DModel as Transformer2DModelOflow
-from .spatio_temporal_oflow import SpatioTemporalResBlock as SpatioTemporalResBlockOflow
-from .spatio_temporal_oflow import TemporalDecoder as TemporalDecoderOflow
-from .spatio_temporal_oflow import (
-    TransformerSpatioTemporalModel as TransformerSpatioTemporalModelOflow,
-)
-from .spatio_temporal_oflow import (
-    TemporalBasicTransformerBlock as TemporalBasicTransformerBlockOflow,
-)
-from .spatio_temporal_oflow import (
-    UNetSpatioTemporalConditionModel as UNetSpatioTemporalConditionModelOflow,
-)
 
 # For CI
 if diffusers_version >= version.parse("0.24.00"):
