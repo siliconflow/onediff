a70f0ac876704ab665c9852ff5253cdece82c875
WangYi <buaawangyi03@gmail.com>
2024-03-26 11:04:17 +0800

    override send_model_to_cpu to prevent changing of data_ptr of cuda tensor

--- onediff_sd_webui_extensions/scripts/onediff.py
+++ onediff_sd_webui_extensions/scripts/onediff.py
@@ -7,11 +7,13 @@ import modules.scripts as scripts
 import modules.shared as shared
 from modules.sd_models import select_checkpoint
 from modules.processing import process_images
+from modules import script_callbacks
 
 from compile_ldm import compile_ldm_unet, SD21CompileCtx
 from compile_sgm import compile_sgm_unet
 from compile_vae import VaeCompileCtx
 from onediff_lora import HijackLoraActivate
+from onediff_hijack import do_hijack as onediff_do_hijack
 
 from onediff.infer_compiler.utils.log_utils import logger
 from onediff.infer_compiler.utils.env_var import parse_boolean_from_env
@@ -204,3 +206,60 @@ class Script(scripts.Script):
         with UnetCompileCtx(), VaeCompileCtx(), SD21CompileCtx(), HijackLoraActivate():
             proc = process_images(p)
         return proc
+
+def unload_model_weights(sd_model=None, info=None):
+    from modules import lowvram, devices
+    m = sd_model or shared.sd_model
+    if m.lowvram:
+        lowvram.send_everything_to_cpu()
+    else:
+        m.to(devices.cpu)
+    devices.torch_gc()
+    return sd_model
+
+def send_model_to_cpu(m):
+    # do nothing
+    pass
+
+def hijack_function(module, name, new_name, new_value):
+    # restore original function in case of reload
+    unhijack_function(module=module, name=name, new_name=new_name)
+    setattr(module, new_name, getattr(module, name))
+    setattr(module, name, new_value)
+
+
+def unhijack_function(module, name, new_name):
+    if hasattr(module, new_name):
+        setattr(module, name, getattr(module, new_name))
+        delattr(module, new_name)
+
+def do_hijack():
+    from modules import sd_models
+    script_callbacks.on_script_unloaded(undo_hijack)
+    hijack_function(
+        module=sd_models,
+        name='unload_model_weights',
+        new_name='__onediff_original_unload_model_weights',
+        new_value=unload_model_weights,
+    )
+    hijack_function(
+        module=sd_models,
+        name='send_model_to_cpu',
+        new_name='__onediff_original_send_model_to_cpu',
+        new_value=send_model_to_cpu,
+    )
+
+def undo_hijack():
+    from modules import sd_models
+    unhijack_function(
+        module=sd_models,
+        name='unload_model_weights',
+        new_name='__onediff_original_unload_model_weights',
+    )
+    unhijack_function(
+        module=sd_models,
+        name='send_model_to_cpu',
+        new_name='__onediff_original_send_model_to_cpu',
+    )
+
+onediff_do_hijack()
