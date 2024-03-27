30736081d78832df061afaee5d7867c50b5cc9cc
FengWen <109639975+ccssu@users.noreply.github.com>
2024-03-27 14:31:34 +0800

    Update _config.py

--- onediff_comfy_nodes/modules/hijack_ipadapter_plus/_config.py
+++ onediff_comfy_nodes/modules/hijack_ipadapter_plus/_config.py
@@ -20,5 +20,5 @@ except Exception as e:
     print(traceback.format_exc())
     print(f"Warning Failed to load {pkg_root} due to {e}")
     is_load_ipadapter_plus_pkg = False
-    import pdb;pdb.set_trace()
+   
 ipadapter_plus_hijacker = Hijacker()
