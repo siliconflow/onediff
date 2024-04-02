c0e77574e4dbf8de1b86709d40f28399a3241cc0
ccsuu <ccsuwen@gmail.com>
2024-04-02 04:54:31 +0000

    Fix: Change ComfyUI-AnimateDiff-Evolved load to warning.

--- onediff_comfy_nodes/modules/hijack_animatediff/_config.py
+++ onediff_comfy_nodes/modules/hijack_animatediff/_config.py
@@ -6,17 +6,19 @@ import os
 from ..sd_hijack_utils import Hijacker
 from onediff.infer_compiler.transform import transform_mgr
 from onediff.infer_compiler.import_tools import DynamicModuleLoader
-from onediff.infer_compiler.utils.log_utils import logger
 COMFYUI_ROOT = os.getenv("COMFYUI_ROOT")
 pkg_name = "ComfyUI-AnimateDiff-Evolved"
 animatediff_root = os.path.join(COMFYUI_ROOT, "custom_nodes", pkg_name)
 load_animatediff_package = True
 try:
-    animatediff_pt = DynamicModuleLoader.from_path(animatediff_root)
-    animatediff_of = transform_mgr.transform_package(pkg_name)
-    comfy_of = transform_mgr.transform_package("comfy")
+    if os.path.exists(animatediff_root):
+        animatediff_pt = DynamicModuleLoader.from_path(animatediff_root)
+        animatediff_of = transform_mgr.transform_package(pkg_name)
+        comfy_of = transform_mgr.transform_package("comfy")
+    else:
+        load_animatediff_package = False
 except Exception as e:
-   logger.warning(f"Failed to load {pkg_name} from {animatediff_root} due to {e}")
+   print(f"Waring: Failed to load {pkg_name} from {animatediff_root} due to {e}")
    load_animatediff_package = False
 
 animatediff_hijacker = Hijacker()
