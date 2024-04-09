a19ee22df22bcd0ad817b8b7c1ef6eab4a2925de
lijunliang <mlbyljl@gmail.com>
2024-04-09 07:36:43 +0000

    fix serverl

---
+++ tests/test_quantitative_quality.py
@@ -0,0 +1,19 @@
+from skimage.metrics import structural_similarity as ssim
+from PIL import Image
+import numpy as np
+import sys
+import unittest
+
+class  QuantizeQuality(unittest.TestCase):
+
+    def validate():
+        ssim = 0
+        image1 = np.array(Image.open('/share_nfs/civitai/20240407-163408.jpg').convert('RGB'))
+        image2 = np.array(Image.open('/src/onediff/output_enterprise_sd.png').convert('RGB'))
+        # Calculate SSIM
+        ssim_index = ssim(image1, image2, multichannel=True, win_size=3)
+        print("SSIM:", ssim_index)
+        self.assertTrue(ssim_index > 0.89,  "SSIM Validation fails, and the workflow is aborted")
+
+if __name__ == "__main__":
+    unittest.main()
