0af2db61ee1c4296c0ba2b65a9b907b580e6ff8c
lijunliang <mlbyljl@gmail.com>
2024-04-09 10:22:35 +0000

    remove ssim

--- tests/test_quantitative_quality.py
+++ tests/test_quantitative_quality.py
@@ -7,13 +7,12 @@ import unittest
 class  QuantizeQuality(unittest.TestCase):
 
     def test_validate(self):
-        ssim = 0
         image1 = np.array(Image.open('/share_nfs/civitai/20240407-163408.jpg').convert('RGB'))
         image2 = np.array(Image.open('/src/onediff/output_enterprise_sd.png').convert('RGB'))
         # Calculate SSIM
         ssim_index = ssim(image1, image2, multichannel=True, win_size=3)
-        print("SSIM:", ssim_index)
         self.assertTrue(ssim_index > 0.89,  "SSIM Validation fails, and the workflow is aborted")
+        print("SSIM:", ssim_index)
 
 if __name__ == "__main__":
     unittest.main()
