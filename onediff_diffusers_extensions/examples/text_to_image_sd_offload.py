2893e225d628a85b85ac4cacafe532eb38294aa7
strint <xiaoyulink@gmail.com>
2024-04-09 11:01:12 +0000

    sd 1.5 test passed

--- onediff_diffusers_extensions/examples/text_to_image_sd_offload.py
+++ onediff_diffusers_extensions/examples/text_to_image_sd_offload.py
@@ -1,6 +1,9 @@
 """
-Torch run example: python examples/text_to_image_sdxl.py
-Compile to oneflow graph example: python examples/text_to_image_sdxl.py
+# run sd 1.5
+ONEDIFF_DEBUG=1 python text_to_image_sd_offload.py --base /share_nfs/hf_models/stable-diffusion-v1-5
+
+# run sdxl
+ONEDIFF_DEBUG=1 python text_to_image_sd_offload.py --base /share_nfs/hf_models/stable-diffusion-xl-base-1.0
 """
 import os
 import argparse
@@ -10,7 +13,7 @@ import torch
 
 from onediff.infer_compiler import oneflow_compile
 from onediff.schedulers import EulerDiscreteScheduler
-from diffusers import StableDiffusionXLPipeline
+from diffusers import AutoPipelineForText2Image
 
 from onediff.infer_compiler.utils.cost_util import cost_cnt
 
@@ -27,7 +30,7 @@ parser.add_argument(
 parser.add_argument("--height", type=int, default=512)
 parser.add_argument("--width", type=int, default=512)
 parser.add_argument("--n_steps", type=int, default=10)
-parser.add_argument("--saved_image", type=str, required=False, default="sdxl-out.png")
+parser.add_argument("--saved_image", type=str, required=False, default="sd-out.png")
 parser.add_argument("--seed", type=int, default=1)
 parser.add_argument(
     "--compile_unet",
@@ -41,12 +44,11 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-# Normal SDXL pipeline init.
+# Normal SD pipeline init.
 OUTPUT_TYPE = "pil"
 
-# SDXL base: StableDiffusionXLPipeline
 scheduler = EulerDiscreteScheduler.from_pretrained(args.base, subfolder="scheduler")
-base = StableDiffusionXLPipeline.from_pretrained(
+base = AutoPipelineForText2Image.from_pretrained(
     args.base,
     scheduler=scheduler,
     torch_dtype=torch.float16,
@@ -77,10 +79,10 @@ image = base(
     output_type=OUTPUT_TYPE,
 ).images
 
-# Normal SDXL run
+# Normal SD run
 @cost_cnt(True)
 def run():
-    print("Normal SDXL run...")
+    print("Normal SD run...")
     torch.manual_seed(args.seed)
     image = base(
         prompt=args.prompt,
