6d2b469e4d48120250d0566c100bf4482f2ae70f
Yao Chi <later@usopp.net>
2024-04-10 10:16:50 +0800

    Update tests/test_quantitative_quality.py

--- tests/test_quantitative_quality.py
+++ tests/test_quantitative_quality.py
@@ -10,7 +10,7 @@ class  QuantizeQuality(unittest.TestCase):
         image2 = np.array(Image.open('/src/onediff/output_enterprise_sd.png').convert('RGB'))
         # Calculate SSIM
         ssim_index = ssim(image1, image2, multichannel=True, win_size=3)
-        print("SSIM:", ssim_index)
+        print("SSIM: ", ssim_index)
         self.assertTrue(ssim_index > 0.89,  "SSIM Validation fails, and the workflow is aborted")
         
 
