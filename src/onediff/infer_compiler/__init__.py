c4d3370b2036d1384c8182dbe9d46599aeb0af5a
hjchen2 <chenhoujiangcug@gmail.com>
2024-04-15 12:24:32 +0800

    refine

--- src/onediff/infer_compiler/__init__.py
+++ src/onediff/infer_compiler/__init__.py
@@ -3,6 +3,7 @@ import torch
 
 from .core import *
 from .utils import set_default_env_vars
+from .utils.options import CompileOptions
 from .utils.options import _GLOBAL_compile_options as compile_options
 
 
