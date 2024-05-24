3b4269f16f71fe9c1f7f8809d53ae1ee5accabf3
lixiang007666 <88304454@qq.com>
2024-05-24 15:50:47 +0800

    [WIP]AAdd nexfort backend dynamic shape example

--- onediff_diffusers_extensions/examples/text_to_image_sdxl.py
+++ onediff_diffusers_extensions/examples/text_to_image_sdxl.py
@@ -1,15 +1,20 @@
 """
 Torch run example: python examples/text_to_image_sdxl.py
-Compile to oneflow graph example: python examples/text_to_image_sdxl.py
+Compile with oneflow: python examples/text_to_image_sdxl.py --compiler oneflow
+Compile with nexfort: python examples/text_to_image_sdxl.py --compiler nexfort
+Test dynamic shape: Add --run_multiple_resolutions 1 and --run_rare_resolutions 1
 """
 import os
+import json
+import time
 import argparse
 
 import torch
 import oneflow as flow
 
-from onediff.infer_compiler import oneflow_compile
+# from onediff.infer_compiler import oneflow_compile
 from onediff.schedulers import EulerDiscreteScheduler
+from onediffx import compile_pipe, CompileOptions
 from diffusers import StableDiffusionXLPipeline
 
 parser = argparse.ArgumentParser()
@@ -22,20 +27,31 @@ parser.add_argument(
     type=str,
     default="street style, detailed, raw photo, woman, face, shot on CineStill 800T",
 )
-parser.add_argument("--height", type=int, default=1024)
-parser.add_argument("--width", type=int, default=1024)
+parser.add_argument("--height", type=int, default=512)
+parser.add_argument("--width", type=int, default=768)
 parser.add_argument("--n_steps", type=int, default=30)
 parser.add_argument("--saved_image", type=str, required=False, default="sdxl-out.png")
 parser.add_argument("--seed", type=int, default=1)
+# parser.add_argument(
+#     "--compile_unet",
+#     type=(lambda x: str(x).lower() in ["true", "1", "yes"]),
+#     default=True,
+# )
+# parser.add_argument(
+#     "--compile_vae",
+#     type=(lambda x: str(x).lower() in ["true", "1", "yes"]),
+#     default=True,
+# )
 parser.add_argument(
-    "--compile_unet",
-    type=(lambda x: str(x).lower() in ["true", "1", "yes"]),
-    default=True,
+    "--compiler",
+    type=str,
+    default="oneflow",
+    choices=["oneflow", "nexfort"],
 )
 parser.add_argument(
-    "--compile_vae",
-    type=(lambda x: str(x).lower() in ["true", "1", "yes"]),
-    default=True,
+    "--compiler-config",
+    type=str,
+    default=None,
 )
 parser.add_argument(
     "--run_multiple_resolutions",
@@ -63,15 +79,31 @@ base = StableDiffusionXLPipeline.from_pretrained(
 )
 base.to("cuda")
 
-# Compile unet with oneflow
-if args.compile_unet:
-    print("Compiling unet with oneflow.")
-    base.unet = oneflow_compile(base.unet)
+# # Compile unet with oneflow
+# if args.compile_unet:
+#     print("Compiling unet with oneflow.")
+#     base.unet = oneflow_compile(base.unet)
 
-# Compile vae with oneflow
-if args.compile_vae:
-    print("Compiling vae with oneflow.")
-    base.vae.decoder = oneflow_compile(base.vae.decoder)
+# # Compile vae with oneflow
+# if args.compile_vae:
+#     print("Compiling vae with oneflow.")
+#     base.vae.decoder = oneflow_compile(base.vae.decoder)
+
+# Compile the pipe
+if args.compiler == "oneflow":
+    base = compile_pipe(base)
+elif args.compiler == "nexfort":
+    options = CompileOptions()
+    if args.compiler_config is not None:
+        options.nexfort = json.loads(args.compiler_config)
+    else:
+        options.nexfort = json.loads(
+            '{"mode": "max-autotune:cudagraphs", "dynamic": true}'
+        )
+
+    base = compile_pipe(
+        base, backend="nexfort", options=options, fuse_qkv_projections=True
+    )
 
 # Warmup with run
 # Will do compilatioin in the first run
@@ -88,6 +120,8 @@ image = base(
 # Normal SDXL run
 print("Normal SDXL run...")
 torch.manual_seed(args.seed)
+print(f"Running at resolution: {args.height}x{args.width}")
+start_time = time.time()
 image = base(
     prompt=args.prompt,
     height=args.height,
@@ -95,6 +129,8 @@ image = base(
     num_inference_steps=args.n_steps,
     output_type=OUTPUT_TYPE,
 ).images
+end_time = time.time()
+print(f"Inference time: {end_time - start_time:.2f} seconds")
 image[0].save(f"h{args.height}-w{args.width}-{args.saved_image}")
 
 
@@ -106,6 +142,8 @@ if args.run_multiple_resolutions:
         sizes = [360]
     for h in sizes:
         for w in sizes:
+            print(f"Running at resolution: {h}x{w}")
+            start_time = time.time()
             image = base(
                 prompt=args.prompt,
                 height=h,
@@ -113,12 +151,16 @@ if args.run_multiple_resolutions:
                 num_inference_steps=args.n_steps,
                 output_type=OUTPUT_TYPE,
             ).images
+            end_time = time.time()
+            print(f"Inference time: {end_time - start_time:.2f} seconds")
 
 
 if args.run_rare_resolutions:
     print("Test run with other another uncommon resolution...")
     h = 544
     w = 408
+    print(f"Running at resolution: {h}x{w}")
+    start_time = time.time()
     image = base(
         prompt=args.prompt,
         height=h,
@@ -126,3 +168,5 @@ if args.run_rare_resolutions:
         num_inference_steps=args.n_steps,
         output_type=OUTPUT_TYPE,
     ).images
+    end_time = time.time()
+    print(f"Inference time: {end_time - start_time:.2f} seconds")
