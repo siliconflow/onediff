5d5f1deedd8ad4aa65393202c3874db42d7d90bd
strint <xiaoyulink@gmail.com>
2024-05-24 18:03:20 +0800

    rm useless

--- src/onediff/infer_compiler/backends/nexfort/nexfort.py
+++ src/onediff/infer_compiler/backends/nexfort/nexfort.py
@@ -6,15 +6,12 @@ from ..registry import register_backend
 @register_backend("nexfort")
 def compile(torch_module: torch.nn.Module, *, options=None):
     from nexfort.compilers import nexfort_compile
-
-    # from .deployable_module import NexfortDeployableModule
-
     if isinstance(options, str):
         import json
 
+        # TODO(): using jsonschema to define the options schema
         options = json.loads(options)
 
     nexfort_options = options if options is not None else dict()
     compiled_model = nexfort_compile(torch_module, **nexfort_options)
-    # return NexfortDeployableModule(compiled_model, torch_module)
     return compiled_model
