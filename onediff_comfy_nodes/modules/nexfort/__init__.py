c0c0b4e135d528bb4959843fb7e329dfc898fa04
FengWen <ccsuwen@gmail.com>
2024-06-06 17:50:33 +0800

    support pulid

--- onediff_comfy_nodes/modules/nexfort/__init__.py
+++ onediff_comfy_nodes/modules/nexfort/__init__.py
@@ -1,5 +1,6 @@
 from .hijack_samplers import samplers_hijack
 from .hijack_ipadapter_plus import ipadapter_plus_hijacker
-
+from .hijack_pulid_comfyui import pulid_comfyui_hijacker
 samplers_hijack.hijack(last=False)
 ipadapter_plus_hijacker.hijack(last=False)
+pulid_comfyui_hijacker.hijack(last=False)
