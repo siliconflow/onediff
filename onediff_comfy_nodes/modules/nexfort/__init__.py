15b3900e3ab2805a5d364230655f3a7a5aca47a0
FengWen <ccsuwen@gmail.com>
2024-06-12 17:11:41 +0800

    fix Quick Switching checkpoint

--- onediff_comfy_nodes/modules/nexfort/__init__.py
+++ onediff_comfy_nodes/modules/nexfort/__init__.py
@@ -1,3 +1,4 @@
+import os
 from .hijack_samplers import samplers_hijack
 from .hijack_ipadapter_plus import ipadapter_plus_hijacker
 from .hijack_pulid_comfyui import pulid_comfyui_hijacker
@@ -7,3 +8,8 @@ samplers_hijack.hijack(last=False)
 ipadapter_plus_hijacker.hijack(last=False)
 pulid_comfyui_hijacker.hijack(last=False)
 comfyui_instantid_hijacker.hijack(last=False)
+
+
+# https://github.com/pytorch/pytorch/blob/1edcb31d34ef012d828bb9f39a8aef6020f580b2/aten/src/ATen/cuda/CUDABlas.cpp#L182-L203
+if os.getenv("CUBLASLT_WORKSPACE_SIZE") is None:
+    os.environ["CUBLASLT_WORKSPACE_SIZE"] = str(1024 * 1024)
