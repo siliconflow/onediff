d429f9729aaac2fd14161dd58ad70c1f77099646
ccsuu <ccsuwen@gmail.com>
2024-06-23 08:07:26 +0000

    debug ci

--- onediff_comfy_nodes/benchmarks/scripts/install_env.sh
+++ onediff_comfy_nodes/benchmarks/scripts/install_env.sh
@@ -17,10 +17,10 @@ fi
 ln -s /share_nfs/hf_models/comfyui_resources/custom_nodes/* $CUSTOM_NODES/
 
 echo "Installing dependencies..."
-pip install -r $COMFYUI_ROOT/requirements.txt
-pip install websocket-client==1.8.0
+pip install -r $COMFYUI_ROOT/requirements.txt --user
+pip install websocket-client==1.8.0 -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install pynvml==11.5.0
-pip install numpy==1.26.4
+# pip install numpy==1.26.4
 pip install scikit-image
 
 # pip install -r $CUSTOM_NODES/ComfyUI_InstantID/requirements.txt
--- onediff_comfy_nodes/benchmarks/scripts/run_oneflow_case.sh
+++ onediff_comfy_nodes/benchmarks/scripts/run_oneflow_case.sh
@@ -14,19 +14,19 @@ COMFY_PORT=8188
 #     --output-images 
 python3 scripts/text_to_image.py \
     --comfy-port $COMFY_PORT \
-    -w resources/oneflow/lora_speedup.json resources/oneflow/lora_multiple_speedup.json \
-    --baseline-dir $STANDARD_OUTPUT/test_lora_speedup 
+    -w resources/oneflow/lora_speedup.json resources/oneflow/lora_multiple_speedup.json resources/oneflow/ComfyUI_IPAdapter_plus/ipadapter_advanced.json 
+    # --baseline-dir $STANDARD_OUTPUT/test_lora_speedup 
 
 # # Baseline 
 # python3 scripts/text_to_image.py \
 #      --comfy-port $COMFY_PORT \ 
 #     -w resources/baseline/ComfyUI_IPAdapter_plus/ipadapter_advanced.json \
 #     --output-images
-python3 scripts/text_to_image.py \
-    --comfy-port $COMFY_PORT \
-    -w resources/oneflow/ComfyUI_IPAdapter_plus/ipadapter_advanced.json \
-    --output-images \
-    --baseline-dir $STANDARD_OUTPUT/test_ipa
+# python3 scripts/text_to_image.py \
+    # --comfy-port $COMFY_PORT \
+    # -w resources/oneflow/ComfyUI_IPAdapter_plus/ipadapter_advanced.json \
+    # --output-images \
+    # --baseline-dir $STANDARD_OUTPUT/test_ipa
 
 
 # # # # Baseline 
