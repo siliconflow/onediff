e9477f43304eeed4869ccde3a57c091602232648
FengWen <109639975+ccssu@users.noreply.github.com>
2024-07-09 03:07:53 +0800

    Auto-Sync Graph Tensor on Weight Update (#1000)
    修复： https://github.com/siliconflow/sd-team/issues/423
    
    - [x] 添加测试

--- src/onediff/infer_compiler/backends/oneflow/deployable_module.py
+++ src/onediff/infer_compiler/backends/oneflow/deployable_module.py
@@ -15,7 +15,12 @@ from .transform.builtin_transform import torch2oflow
 from .dual_module import DualModule, get_mixed_dual_module
 from .oneflow_exec_mode import oneflow_exec_mode, oneflow_exec_mode_enabled
 from .args_tree_util import input_output_processor
-from .param_utils import parse_device, check_device, generate_constant_folding_info
+from .param_utils import (
+    parse_device,
+    check_device,
+    generate_constant_folding_info,
+    update_graph_with_constant_folding_info,
+)
 from .graph_management_utils import graph_file_management
 from .online_quantization_utils import quantize_and_deploy_wrapper
 from ..env_var import OneflowCompileOptions
@@ -195,6 +200,7 @@ class OneflowDeployableModule(DeployableModule):
             file_path, device, run_warmup, state_dict=state_dict
         )
         generate_constant_folding_info(self)
+        update_graph_with_constant_folding_info(self)
 
     def save_graph(self, file_path, *, process_state_dict=lambda x: x):
         self.get_graph().save_graph(file_path, process_state_dict=process_state_dict)
