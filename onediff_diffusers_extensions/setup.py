a6b8b05ce03f347c2fc31cb7613a7c9ba6965c11
Xiaoyu Xu <xiaoyulink@gmail.com>
2024-07-27 20:38:57 +0800

    fix pub version (#1047)
    VERSION_LOCAL_SCHEME=no-local-version python -m build
    
    Successfully built onediff-1.1.1.dev66.tar.gz and
    onediff-1.1.1.dev66-py3-none-any.whl

--- onediff_diffusers_extensions/setup.py
+++ onediff_diffusers_extensions/setup.py
@@ -1,6 +1,9 @@
+import os
+
 from setuptools import find_packages, setup
 
 
+local_scheme = os.getenv("VERSION_LOCAL_SCHEME", "node-and-date")
 setup(
     name="onediffx",
     description="onediff extensions for diffusers",
@@ -37,6 +40,8 @@ setup(
         "root": "../",
         "write_to": "./onediff_diffusers_extensions/onediffx/_version.py",
         "fallback_version": "0.0.0",
+        "version_scheme": "guess-next-dev",
+        "local_scheme": local_scheme,
     },
     setup_requires=["setuptools_scm"],
 )
