027fcf4860d3afb8e5dd24f7b6e4a3652a47f713
strint <xiaoyulink@gmail.com>
2024-08-05 00:04:21 +0800

    add df version

--- onediff_diffusers_extensions/examples/flux/flux.py
+++ onediff_diffusers_extensions/examples/flux/flux.py
@@ -1,17 +1,22 @@
 import torch
-from diffusers import  FluxPipeline
+
+# only works at https://github.com/huggingface/diffusers/commit/ebf3ab1477dd480df1b8dd5d97a7b4aa3822716b
+# ebf3ab1477dd480df1b8dd5d97a7b4aa3822716b
+from diffusers import FluxPipeline
 
 # Reference: https://github.com/huggingface/diffusers/pull/9043
-pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16, revision='refs/pr/1')
+pipe = FluxPipeline.from_pretrained(
+    "black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16, revision="refs/pr/1"
+)
 # pipe.enable_model_cpu_offload()
 
 prompt = "A cat holding a sign that says hello world"
 out = pipe(
-    prompt=prompt, 
-    guidance_scale=0., 
-    height=768, 
-    width=1360, 
-    num_inference_steps=4, 
+    prompt=prompt,
+    guidance_scale=0.0,
+    height=768,
+    width=1360,
+    num_inference_steps=4,
     max_sequence_length=256,
 ).images[0]
 out.save("image.png")
