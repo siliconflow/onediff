bac3c15dd198d24479a9875d87321bfae71cce87
FengWen <ccsuwen@gmail.com>
2024-08-07 13:38:55 +0800

    test

--- onediff_comfy_nodes/benchmarks/scripts/run_nexfort_flux_test.sh
+++ onediff_comfy_nodes/benchmarks/scripts/run_nexfort_flux_test.sh
@@ -10,8 +10,17 @@ python3 scripts/text_to_image.py \
     --comfy-port $COMFY_PORT \
     -w $WORKFLOW_BASIC/flux1-schnell.json \
     --output-dir results \
+    --exp-name flux1-schnell-torch \
+    --output-images
+
+# Run the FLUX1 baseline workflow
+python3 scripts/text_to_image.py \
+    --comfy-port $COMFY_PORT \
+    -w $WORKFLOW_BASIC/flux1-schnell.json \
+    --output-dir results \
     --exp-name flux1-schnell-baseline \
     --output-images
+
 
 # Run the FLUX1 nexfort workflow
 python3 scripts/text_to_image.py \
