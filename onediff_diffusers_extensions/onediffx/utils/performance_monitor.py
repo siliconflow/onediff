a5b6bf2aec0ce22a4fcb4fca759a609625e4c9a3
Li Xiang <54010254+lixiang007666@users.noreply.github.com>
2024-08-09 14:28:27 +0800

    Update performance_monitor.py

--- onediff_diffusers_extensions/onediffx/utils/performance_monitor.py
+++ onediff_diffusers_extensions/onediffx/utils/performance_monitor.py
@@ -1,20 +1,34 @@
+import torch
 import time
 from contextlib import contextmanager
 
-
 @contextmanager
-def track_inference_time(warmup=False):
+def track_inference_time(warmup=False, use_cuda=True):
     """
     A context manager to measure the execution time of models.
     Parameters:
         warmup (bool): If True, prints the time for warmup runs; otherwise, prints the time for normal runs.
+        use_cuda (bool): If CUDA is available, uses torch.cuda.Event for timing; otherwise, uses time.time().
     """
-    try:
+    if use_cuda and torch.cuda.is_available():
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
+        start.record()
+    else:
         start_time = time.time()
+    
+    try:
         yield
     finally:
-        end_time = time.time()
-        if warmup:
-            print(f"Warmup run - Execution time: {end_time - start_time:.2f} seconds")
+        if use_cuda and torch.cuda.is_available():
+            end.record()
+            torch.cuda.synchronize()
+            elapsed_time = start.elapsed_time(end) / 1000.0
         else:
-            print(f"Normal run - Execution time: {end_time - start_time:.2f} seconds")
+            elapsed_time = time.time() - start_time
+        
+        if warmup:
+            print(f"Warmup run - Execution time: {elapsed_time:.2f} seconds")
+        else:
+            print(f"Normal run - Execution time: {elapsed_time:.2f} seconds")
+
